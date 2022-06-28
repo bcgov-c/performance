8979aa159fa9a74f7ea669632e9621aea77e85ab
Amabella Cinco <amabella.cinco@telus.com>
2022-06-27 17:50:27 -0700

    #578
    Text Edits - Sys Admin and HR Admin
    All except change son Excuse Employees

--- resources/views/shared/goalbank/partials/tabs.blade.php
+++ resources/views/shared/goalbank/partials/tabs.blade.php
@@ -1,7 +1,7 @@
 <div class="d-flex justify-content-center justify-content-lg-start mb-2" role="tablist">
     <div class="px-4 py-1 mr-2 border-bottom {{Route::current()->getName() == request()->segment(1).'.goalbank.createindex' ? 'border-primary' : ''}}">
         <x-button role="tab" :href="route(request()->segment(1).'.goalbank.createindex')" style="">
-          Add a New Goal in Goal Bank
+          Add a New Goal to the Goal Bank
         </x-button>
     </div>
     <div class="px-4 py-1 mr-2 border-bottom {{Route::current()->getName() == request()->segment(1).'.goalbank.manageindex' ? 'border-primary' : ''}}">
