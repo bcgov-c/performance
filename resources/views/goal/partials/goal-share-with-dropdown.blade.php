6fc7465ea131906092fd36e16588f3a36739130a
Amabella Cinco <amabella.cinco@telus.com>
2022-06-28 17:48:50 -0700

    #488 Tooltips fixes
    Shared with tooltip (position-right)

--- resources/views/goal/partials/goal-share-with-dropdown.blade.php
+++ resources/views/goal/partials/goal-share-with-dropdown.blade.php
@@ -6,12 +6,12 @@
     > -->
     <label class="d-flex justify-content-left align-items-center" style="font-weight: normal;" 
     @if(!(isset($doNotShowInfo) && $doNotShowInfo)) 
-        data-trigger="click" data-toggle="popover" data-placement="left" data-html="true"  
+        data-trigger="click" data-toggle="popover" data-placement="right" data-html="true"  
     @endif
     >    
     {{ $shareWithLabel ?? "Shared with"}}:&nbsp; 
     @if(!(isset($doNotShowInfo) && $doNotShowInfo))
-        <i class="fa fa-info-circle"data-trigger="click" data-toggle="popover" data-placement="left" data-html="true" data-content="By default, all of your goals are private. Use the &quot;Share with&quot; dropdown menu to make a goal visible to selected employees. This lets team members know what you are working on and may help team members set their own goals." >
+        <i class="fa fa-info-circle"data-trigger="click" data-toggle="popover" data-placement="right" data-html="true" data-content="By default, all of your goals are private. Use the &quot;Share with&quot; dropdown menu to make a goal visible to selected employees. This lets team members know what you are working on and may help team members set their own goals." >
         </i>
         &nbsp;
     @endif
