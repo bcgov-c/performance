826376cc8979c95d6eb7ba51c209c52de39778af
Amabella Cinco <amabella.cinco@telus.com>
2022-06-29 19:20:42 -0700

    #488 Tooltips fixes
    My Goal > Create New Goal and Edit Goal
    Toggle tips close upon click of anywhere else in the app, outside of the toggle tip window.

--- resources/views/goal/index.blade.php
+++ resources/views/goal/index.blade.php
@@ -65,28 +65,49 @@
             @csrf
             <div class="row">
                 <div class="col-6">
-
-                    <x-tooltip-dropdown-outside name="goal_type_id" :options="$goaltypes" label="Goal Type" popoverstr="{{$type_desc_str}}" tooltipField="description" displayField="name" />
+                    <div>
+                        <b>Goal Type</b>
+                        <i class="fa fa-info-circle" data-trigger='click' data-toggle="popover" data-placement="right" data-html="true" data-content="{{$type_desc_str}}"> </i>
+                        <!-- <x-tooltip-dropdown-outside name="goal_type_id" :options="$goaltypes" label="Goal Type" popoverstr="{{$type_desc_str}}" tooltipField="description" displayField="name" />                         -->
+                        <x-dropdown :list="$goaltypes" name="goal_type_id" />
+                    </div>
                     </div>
                        <div class="col-6">
-                    <x-input-modal label="Goal Title" id="goal_title" name="title" tooltip='A short title (1-3 words) used to reference the goal throughout the Performance Development Platform.' />
+                       <b>Goal Title</b>
+                        <i class="fa fa-info-circle" data-trigger='click' data-toggle="popover" data-placement="right" data-html="true" data-content="A short title (1-3 words) used to reference the goal throughout the Performance Development Platform."> </i>                        
+                        <!-- <x-input-modal label="Goal Title" id="goal_title" name="title" tooltip='A short title (1-3 words) used to reference the goal throughout the Performance Development Platform.' /> -->
+                        <x-input-modal id="goal_title" name="title" />
                     </div>
                     <div class="col-sm-6">
-                        <x-xdropdown :list="$tags" label="Tags" name="tag_ids[]"  class="tags" tooltipField="description" displayField="name" multiple/>
+                        <b>Tags</b>
+                        <i class="fa fa-info-circle" id="tags_label" data-trigger='click' data-toggle="popover" data-placement="right" data-html="true" data-content="Tags help to more accurately identity, sort, and report on your goals. You can add more than one tag to a goal. The list of tags will change and grow over time. <br/><br/><a href='/resource/goal-setting?t=4' target=\'_blank\'><u>View full list of tag descriptions.</u></a>"></i>				
+                        <!-- <x-xdropdown :list="$tags" label="Tags" name="tag_ids[]"  class="tags" tooltipField="description" displayField="name" multiple/> -->
+                        <x-xdropdown :list="$tags" name="tag_ids[]"  class="tags" displayField="name" multiple/>
                         <small  class="text-danger error-tag_ids"></small>
                     </div>
                        <div class="col-12">
-                        <label style="font-weight: normal;">
-                            <b>Goal Description</b>
-                            <p class="py-2">Each goal should include a description of <b>WHAT</b><x-tooltip-modal text='A concise opening statement of what you plan to achieve. For example, "My goal is to deliver informative Performance Development sessions to ministry audiences".' /> you will accomplish, <b>WHY</b><x-tooltip-modal text='Why this goal is important to you and the organization (value of achievement). For example, "This will improve the consistency and quality of the employee experience across the BCPS".' /> it is important, and <b>HOW</b><x-tooltip-modal text='A few high level steps to achieve your goal. For example, "I will do this by working closely with ministry colleagues to develop presentations that respond to the needs of their employees in each aspect of the Performance Development process".'/> you will achieve it.</p>
-                            <textarea id="what" label="Goal Description" name="what" ></textarea>
-                            <small class="text-danger error-what"></small>
-                        </label>
+                        <!-- <label style="font-weight: normal;"> -->
+                        <b>Goal Description</b>          
+                        <p>
+				        Each goal should include a description of <b>WHAT</b>  
+				        <i class="fa fa-info-circle" data-trigger="click" data-toggle="popover" data-placement="right" data-html="true" data-content='A concise opening statement of what you plan to achieve. For example, "My goal is to deliver informative Performance Development sessions to ministry audiences".'> </i> you will accomplish, <b>WHY</b> 
+				        <i class="fa fa-info-circle" data-trigger="click" data-toggle="popover" data-placement="right" data-html="true" data-content='Why this goal is important to you and the organization (value of achievement). For example, "This will improve the consistency and quality of the employee experience across the BCPS".'> </i> it is important,, and <b>HOW</b> 
+				        <i class="fa fa-info-circle" data-trigger="click" data-toggle="popover" data-placement="right" data-html="true" data-content='A few high level steps to achieve your goal. For example, "I will do this by working closely with ministry colleagues to develop presentations that respond to the needs of their employees in each aspect of the Performance Development process".'> </i> you will achieve it. 
+				        </p>                                                                  
+                        <!-- <p class="py-2">Each goal should include a description of <b>WHAT</b><x-tooltip-modal text='A concise opening statement of what you plan to achieve. For example, "My goal is to deliver informative Performance Development sessions to ministry audiences".' /> you will accomplish, <b>WHY</b><x-tooltip-modal text='Why this goal is important to you and the organization (value of achievement). For example, "This will improve the consistency and quality of the employee experience across the BCPS".' /> it is important, and <b>HOW</b><x-tooltip-modal text='A few high level steps to achieve your goal. For example, "I will do this by working closely with ministry colleagues to develop presentations that respond to the needs of their employees in each aspect of the Performance Development process".'/> you will achieve it.</p>                                                                 -->
+                        <!-- <textarea id="what" label="Goal Descriptionxxx" name="what" ></textarea> -->
+                        <!-- <textarea id="what" name="what" ></textarea>                             -->
+                        <small class="text-danger error-what"></small>
+                        <x-textarea-modal id="what" name="what" />
+                        <!-- </label> -->
                   </div>
                        <div class="col-12">
-                    <x-textarea-modal id="measure_of_success" label="Measures of Success" name="measure_of_success" tooltip='A qualitative or quantitative measure of success for your goal. For example, "Deliver a minimum of 2 sessions per month that reach at least 100 people"'  />
-                    <small class="text-danger error-measure_of_success"></small>
-                </div>
+                            <b>Measures of Success</b>
+                            <i class="fa fa-info-circle" data-trigger='click' data-toggle="popover" data-placement="right" data-html="true" data-content='A qualitative or quantitative measure of success for your goal. For example, "Deliver a minimum of 2 sessions per month that reach at least 100 people"'> </i>                        
+                            <!-- <x-textarea-modal id="measure_of_success" label="Measures of Success" name="measure_of_success" tooltip='A qualitative or quantitative measure of success for your goal. For example, "Deliver a minimum of 2 sessions per month that reach at least 100 people"'  /> -->
+                            <x-textarea-modal id="measure_of_success" name="measure_of_success" />
+                            <small class="text-danger error-measure_of_success"></small>
+                        </div>
                 <div class="col-sm-6">
                     <x-input label="Start Date " class="error-start" type="date" name="start_date" id="start_date" />
                     <small  class="text-danger error-start_date"></small>
@@ -161,7 +182,23 @@
             selector: '[data-toggle-select]',
             trigger: 'click',
         });
+
+        $(".tags").multiselect({
+                	enableFiltering: true,
+                	enableCaseInsensitiveFiltering: true,
+					nonSelectedText: null,
+            	});
+
+				$('body').on('click', function (e) {
+                $('[data-toggle=popover]').each(function () {
+                    // hide any open popovers when the anywhere else in the body is clicked
+                    	if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                        $(this).popover('hide');
+                    	}
+                	});
+				});
         
+
 /* 
     $('select[name="goal_type_id"]').trigger('change');
 
