7c5333797066cb4775cac041f1f7da07b1fae543
Amabella Cinco <amabella.cinco@telus.com>
2022-07-05 15:20:49 -0700

    #488 tooltips fixes
    MyGoals>My Current Goals>  Shared with tooltip

--- resources/views/goal/partials/goal-share-with-dropdown.blade.php
+++ resources/views/goal/partials/goal-share-with-dropdown.blade.php
@@ -9,9 +9,7 @@
     @if(!(isset($doNotShowInfo) && $doNotShowInfo))
         <i class="fa fa-info-circle" data-trigger="click" data-toggle="popover" data-placement="right" data-html="true" data-content="By default, all of your goals are private. Use the &quot;Share with&quot; dropdown menu to make a goal visible to selected employees. This lets team members know what you are working on and may help team members set their own goals." >
         </i>        
-    @endif
-    
-    </label>
+    @endif    
          
     <select multiple class="form-control search-users ml-1" id="search-users-{{$goal->id}}" name="share_with[{{$goal->id}}][]" data-goal-id="{{$goal->id}}">
         @php
