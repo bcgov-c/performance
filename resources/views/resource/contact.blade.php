9df6d20f86ae4d3e3f053e9edeae1c8795b37fd4
Amabella Cinco <amabella.cinco@telus.com>
2022-07-06 17:15:41 -0700

    #589 Resources Section Contact tab
    contact.blade.php
    clean up email address and link to AskMyHR

--- resources/views/resource/contact.blade.php
+++ resources/views/resource/contact.blade.php
@@ -1,9 +1,8 @@
 @extends('resource.layout')
 @section('tab-content')
 <ul>
-	<li>
-		<!-- For additional support, contact <a href="mailto:travis.clark@gov.bc.ca" target="_blank">Travis Clark  [Travis.Clark@gov.bc.ca], Lead for Performance Management</a>. -->
-		For additional support, email <a href="mailto:Performance.Development@gov.bc.ca" target="_blank">Performance Development  [Performance.Development@gov.bc.ca]</a> or submit a ticket through AskMyHR to My Team or Organization > HR Software Systems Support > Performance Development Platform.		
+	<li>		
+		For additional support, email <a href="mailto:Performance.Development@gov.bc.ca" target="_blank">Performance Development </a> or submit a ticket through <a target="blank" href="https://www2.gov.bc.ca/gov/content/careers-myhr">AskMyHR</a> to My Team or Organization > HR Software Systems Support > Performance Development Platform.		
 	</li>
 </u>
 @endsection
