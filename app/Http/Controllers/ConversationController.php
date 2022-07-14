4e0a3e01a15ace26f6d1f4900698ef908c9d894d
jizhaogit <ji.zhao@telus.com>
2022-07-14 13:42:48 -0400

    #461

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -98,13 +98,8 @@ class ConversationController extends Controller
             });
             
              // With My Team
-            //$myTeamQuery->whereNotIn('user_id', '<>', $supervisorId);
-            $myTeamQuery->where(function($query) use ($sharedSupervisorIds) {
-                $query->WhereHas('conversationParticipants', function ($query) use ($sharedSupervisorIds) {
-                    $query->whereNotIn('participant_id', $sharedSupervisorIds);
-                });
-            });
-            $myTeamQuery->whereNotIn('user_id', $sharedSupervisorIds);
+            $myTeamQuery->whereNotIn('user_id', $sharedSupervisorIds);    
+            $myTeamQuery->where('user_id', '<>', $authId);
             
             $type = 'past';
 
