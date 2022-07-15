85e57f55176bd6dd829e9981bcaba76ab10b7a7c
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-07-15 08:46:27 -0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
     {
 
         // Seeder for Prod Environment
-        $this->call(UserTableSeeder_GoLive::class);
         $this->call(GoalTypeSeeder::class);
         $this->call(GoalTypeSeeder_Update20220607::class);
         $this->call(TopicSeeder::class);
@@ -26,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder_Add_Longnames::class);
         $this->call(SharedElementSeeder::class);
         $this->call(DashboardMessageSeeder::class);
+        $this->call(UserTableSeeder_GoLive::class);
 
         
     }
