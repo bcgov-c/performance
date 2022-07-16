228b1b16c9f20da600ce3fc7b1aa43b31dc848f1
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-07-16 14:25:22 -0700

    Update AzureLoginController.php

--- app/Http/Controllers/Auth/AzureLoginController.php
+++ app/Http/Controllers/Auth/AzureLoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auth\SocialiteBaseController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use App\Models\DashboardMessage;
 
 class AzureLoginController extends SocialiteBaseController {
 
