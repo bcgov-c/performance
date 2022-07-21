f184f950166e2e6c46e65bdd88a79fe5e996b91c
Amabella Cinco <amabella.cinco@telus.com>
2022-07-21 12:52:41 -0700

    #713 Text Edit > Replace "ticket" with "service request"
    Dashboard > My Current Supervisor tooltip
    Resource>Contact
    User Guide>My Team Section (Shared Status)

--- resources/views/resource/contact.blade.php
+++ resources/views/resource/contact.blade.php
@@ -2,7 +2,7 @@
 @section('tab-content')
 <ul>
 	<li>		
-		For additional support, email <a href="mailto:Performance.Development@gov.bc.ca" target="_blank">Performance Development </a> or submit a ticket through <a target="blank" href="https://www2.gov.bc.ca/gov/content/careers-myhr">AskMyHR</a> to My Team or Organization > HR Software Systems Support > Performance Development Platform.		
+		For additional support, email <a href="mailto:Performance.Development@gov.bc.ca" target="_blank">Performance Development </a> or submit a service request through <a target="blank" href="https://www2.gov.bc.ca/gov/content/careers-myhr">AskMyHR</a> to My Team or Organization > HR Software Systems Support > Performance Development Platform.		
 	</li>
 </u>
 @endsection
