927f3b453a4b885643c53fb41abcb26ddeb94884
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-08-29 07:47:03 -0700

    ZenHub #627
    ZenHub #627

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -177,6 +177,26 @@ class Conversation extends Model
 
         $authId = Auth::id();
         $user = User::find($authId);
+
+        $check1 = ($user->excused_start_date !== null);
+        $check2 = ($user->excused_end_date !== null);
+        if($check1 && $check2) {
+            $check3 = ($user->excused_start_date <= $user->excused_end_date);
+            $newDate = new Carbon(Carbon::today()->toDateString());
+            $check4 = ($newDate->between($user->excused_start_date, $user->excused_end_date));
+        } else {
+            $check3 = false;
+            $check4 = false;
+        }
+        if($check1 && $check2 && $check3 && $check4) {
+            $msg = "Employee is excused until ";
+            $endDate = new Carbon($user->excused_end_date);
+            return [
+                // $msg. $user->excused_end_date->format('d-M-y'), "success"
+                $msg. $endDate->format('d-M-y'), "success"
+            ];
+        } 
+        
         if ((session()->get('original-auth-id') == Auth::id() or session()->get('original-auth-id') == null )){
             // $msg1 = "You are required to complete a performance conversation every 4 months at minimum. You are overdue. Please complete a conversation as soon as possible.";
             // $msg2 = "Your next performance conversation is due by ";
@@ -188,23 +208,35 @@ class Conversation extends Model
             // $msg3 = $user->name . " must complete the first performance conversation by ";    
             $msg = "Next performance conversation is due by ";                        
         }
-              
-        
+
+
         if ($lastConv) {
-            if ($lastConv->sign_off_time->addMonths(4)->lt(Carbon::now())) {
-                $nextDueDate = $lastConv->sign_off_time->addMonths(4);    
+
+            if($check1 && $check2) {
+                $check5 = ($lastConv->sign_off_time->addMonths(4)->between($user->excused_start_date, $user->excused_end_date));
+            }
+            $tempNextDueDate = $lastConv->sign_off_time->addMonths(4);    
+            if($check1 && $check2 && $check5) {
+                $newStartDate = new Carbon($user->excused_start_date);
+                $newEndDate = new Carbon($user->excused_end_date);
+                $diffInDays = $tempNextDueDate->diffInDays($newStartDate);
+                $tempNextDueDate->addDays($diffInDays);
+            }
+
+            $nextDueDate = $tempNextDueDate;    
+            if ($tempNextDueDate->lt(Carbon::now())) {
                 return [
                     //  $msg1,   
                     $msg.$nextDueDate->format('d-M-y'),
                     "danger"
                 ];
             }
-            $nextDueDate = $lastConv->sign_off_time->addMonths(4);
-            $diff = Carbon::now()->diffInMonths($lastConv->sign_off_time->addMonths(4), false);
+            // $nextDueDate = $tempNextDueDate;
+            $diff = Carbon::now()->diffInMonths($tempNextDueDate, false);
             return [
                 // "Your last performance conversation was completed on ".$lastConv->sign_off_time->format('d-M-y').". 
                 // $msg2. $lastConv->sign_off_time->addMonths(4)->format('d-M-y'),
-                $msg. $lastConv->sign_off_time->addMonths(4)->format('d-M-y'),
+                $msg. $nextDueDate->format('d-M-y'),
                 $diff < 0 ? "danger" : ($diff < 1 ? "warning" : "success")
             ];
         }
@@ -215,6 +247,16 @@ class Conversation extends Model
         if ((!$nextDueDate) || (Carbon::createFromDate(2022, 10, 14)->gt($nextDueDate))) {
             $DDt = abs (($user->id % 10) - 1) * 5 + (($user->id % 5));
             $nextDueDate = Carbon::createFromDate(2022, 10, 14)->addDays($DDt);
+        }
+
+        if($check1 && $check2) {
+            $check5 = ($nextDueDate->between($user->excused_start_date, $user->excused_end_date));
+        }
+        if($check1 && $check2 && $check5) {
+            $newStartDate = new Carbon($user->excused_start_date);
+            $newEndDate = new Carbon($user->excused_end_date);
+            $diffInDays = $nextDueDate->diffInDays($newStartDate);
+            $newEndDate->addDays($diffInDays);
         }
 
         /* dd([
