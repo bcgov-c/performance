8985b2a8e425f1b514df192ea55ce131364c21e4
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-10-17 00:52:38 -0700

    20221104 to Prod

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -174,42 +174,26 @@ class Conversation extends Model
 
     public static function warningMessage() {
         $lastConv = self::getLastConv();
-
         $authId = Auth::id();
         $user = User::find($authId);
-        if ((session()->get('original-auth-id') == Auth::id() or session()->get('original-auth-id') == null )){
-            // $msg1 = "You are required to complete a performance conversation every 4 months at minimum. You are overdue. Please complete a conversation as soon as possible.";
-            // $msg2 = "Your next performance conversation is due by ";
-            // $msg3 = "You must complete your first performance conversation by ";            
-            $msg = "Next performance conversation is due by ";            
+
+        $jr = EmployeeDemoJunior::where('guid', $user->guid)->getQuery()->orderBy('id', 'desc')->first();
+        if ($jr->excused_type == 'A' || $user->excused_flag) {
+            $msg = "Employee is currently excused and their conversation deadline is paused";
+            return [
+                $msg, "success"
+            ];
         } else {
-            // $msg1 = $user->name . " is required to complete a performance conversation every 4 months at minimum. It is overdue. Please complete a conversation as soon as possible.";
-            // $msg2 = $user->name . "'s next performance conversation is due by ";
-            // $msg3 = $user->name . " must complete the first performance conversation by ";    
-            $msg = "Next performance conversation is due by ";                        
-        }
-              
-        
-        if ($lastConv) {
-            if ($lastConv->sign_off_time->addMonths(4)->lt(Carbon::now())) {
-                $nextDueDate = $lastConv->sign_off_time->addMonths(4);    
+            $msg = "Next performance conversation is due by ";            
+            if (Carbon::now()->gte($jr->next_conversation_date)) {
                 return [
-                    // $msg3 . $nextDueDate->format('d-M-y'),
-                    $msg. $nextDueDate->format('d-M-y'),
-                    $diff < 0 ? "danger" : ($diff < 1 ? "warning" : "success")
-                ];
-            } else {
-                $msg = "Employee is currently excused and their conversation deadline is paused";
-                return [
-                    $msg, "success"
+                    $msg.Carbon::parse($jr->next_conversation_date)->format('M d, Y'),
+                    "danger"
                 ];
             }
-            $nextDueDate = $lastConv->sign_off_time->addMonths(4);
-            $diff = Carbon::now()->diffInMonths($lastConv->sign_off_time->addMonths(4), false);
+            $diff = Carbon::now()->diffInMonths(Carbon::parse($jr->next_conversation_date), false);
             return [
-                // "Your last performance conversation was completed on ".$lastConv->sign_off_time->format('d-M-y').". 
-                // $msg2. $lastConv->sign_off_time->addMonths(4)->format('d-M-y'),
-                $msg. $lastConv->sign_off_time->addMonths(4)->format('d-M-y'),
+                $msg.Carbon::parse($jr->next_conversation_date)->format('M d, Y'),
                 $diff < 0 ? "danger" : ($diff < 1 ? "warning" : "success")
             ];
         }
