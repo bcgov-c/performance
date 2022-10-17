b5c5e2c18993b111fa26952ef6bf9de4e250fe84
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-10-17 08:12:14 -0700

    ZenHub #794
    ZenHub #794

--- resources/views/shared/excuseemployees/partials/tabs.blade.php
+++ resources/views/shared/excuseemployees/partials/tabs.blade.php
@@ -1,7 +1,7 @@
 <div class="d-flex justify-content-center justify-content-lg-start mb-2" role="tablist">
     <div class="px-4 py-1 mr-2 border-bottom {{Route::current()->getName() == request()->segment(1).'.excuseemployees' ? 'border-primary' : ''}}">
         <x-button role="tab" :href="route(request()->segment(1).'.excuseemployees')" style="">
-          Excuse New Employee(s)
+          Managed Excused Status
         </x-button>
     </div>
     <div class="px-4 py-1 mr-2 border-bottom {{Route::current()->getName() == request()->segment(1).'.excuseemployees.manageindex' ? 'border-primary' : ''}}">
