a864688a028483d05342430d4e245c2da9399e93
jp-Telus <james.poon@telus.com>
2022-10-18 21:11:59 -0700

    #803 Conversation due email not received for team members and supervisor (add start script for background process after the conatiner startup)

--- server_files/start.sh
+++ server_files/start.sh
@@ -3,7 +3,7 @@
 cd /var/www/html
 
 # Queue 
-nohup php artisan queue:work --tries=3 --timeout=0 > ./storage/logs/queue-work.log &
+nohup php artisan queue:work  --daemon --tries=3 --timeout=0 > ./storage/logs/queue-work.log < /dev/null > /dev/null 2>&1 &
 
 # Schedule (required manual start)
-nohup php artisan schedule:work --verbose --no-interaction &
+nohup php artisan schedule:work --verbose --no-interaction < /dev/null > /dev/null 2>&1 &
