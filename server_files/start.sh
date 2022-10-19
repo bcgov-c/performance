c06fe26c3b26476d9f33324ed15155cee89c695d
jp-Telus <james.poon@telus.com>
2022-10-18 19:59:03 -0700

    Revert "#803 Conversation due email not received for team members and supervisor (add start script for background process after the conatiner startup )"
    This reverts commit 4c79d32e04d2e948da13a131f6c576c954737629.

--- server_files/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /var/www/html
-
-# Queue 
-nohup php artisan queue:work --tries=3 --timeout=0 > ./storage/logs/queue-work.log &
-
-# Schedule (required manual start)
-nohup php artisan schedule:work --verbose --no-interaction &
