3d5a4ca69b42f05ee05d9389a024ee20d5d2a804
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-10-19 04:33:28 -0700

    Kernel Test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -53,6 +53,10 @@ class Kernel extends ConsoleKernel
         ->timezone('America/Vancouver')
         ->dailyAt('02:00');
 
+        $schedule->command('command:CalcNextConversationDate')
+        ->timezone('America/Vancouver')
+        ->dailyAt('05:00');
+
         $schedule->command('command:NotifyConversationDue')
         ->timezone('America/Vancouver')    
         ->dailyAt('02:30')
