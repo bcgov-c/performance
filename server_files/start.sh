c50e40aaf2a6176709b94c7934ebd82668a45971
jp-Telus <james.poon@telus.com>
2022-10-19 13:53:47 -0700

    #803 Conversation due email not received for team members and supervisor

--- server_files/start.sh
+++ server_files/start.sh
@@ -6,4 +6,4 @@ cd /var/www/html
 nohup php artisan queue:work  --daemon --tries=3 --timeout=0 > ./storage/logs/queue-work.log </dev/null>/dev/null 2>&1 &
 
 # Schedule (required manual start)
-nohup php artisan schedule:work --verbose --no-interaction </dev/null>/dev/null 2>&1 &
+#nohup php artisan schedule:work --verbose --no-interaction </dev/null>/dev/null 2>&1 &
