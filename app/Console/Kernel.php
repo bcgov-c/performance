d1a93bac66ae1dcbeaaf992f6939d560842bf3d8
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-10-19 13:47:41 -0700

    Revert "Test Kernel"
    This reverts commit 12ea0504411c11d8bd59f0ae9a23be0787850b2b.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -53,10 +53,6 @@ class Kernel extends ConsoleKernel
         ->timezone('America/Vancouver')
         ->dailyAt('02:00');
 
-        $schedule->command('command:CalcNextConversationDate')
-        ->timezone('America/Vancouver')
-        ->dailyAt('10:45');
-
         $schedule->command('command:NotifyConversationDue')
         ->timezone('America/Vancouver')    
         ->dailyAt('02:30');
