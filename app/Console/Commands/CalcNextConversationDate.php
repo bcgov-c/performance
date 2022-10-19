881002dfda5e2352735fae656e1986600aa2bb7c
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-10-19 10:25:10 -0700

    ZenHub #772
    ZenHub #772

--- app/Console/Commands/CalcNextConversationDate.php
+++ app/Console/Commands/CalcNextConversationDate.php
@@ -11,7 +11,7 @@ use App\Models\ExcusedClassification;
 use App\Models\JobSchedAudit;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
+// use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 
