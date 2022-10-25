1bdced2369459bf0605ffc7837fe0ccc51fb58b5
jizhaogit <ji.zhao@telus.com>
2022-10-25 11:33:54 -0400

    Revert "#817"
    This reverts commit e9b3e4c75ff96a9bc89f3c1fddefa16de33a4fac.

--- app/Http/Controllers/SysadminController.php
+++ app/Http/Controllers/SysadminController.php
@@ -876,20 +876,6 @@ class SysadminController extends Controller
     } 
     
     public function switchIdentity(Request $request) {
-            $user_info = DB::table('users')
-                    ->select('*')
-                    ->where('name','=','Roberta King')
-                    ->count();
-        
-            echo "users # : " . $user_info;
-            
-            $employee_info = DB::table('users')
-                    ->select('users.*')
-                    ->join('employee_demo', 'employee_demo.guid', '=', 'users.guid')
-                    ->where('users.name','=','Roberta King')
-                    ->count();
-            echo " | employee # : " . $employee_info;
-        
             $user = auth()->user();
             $switched_userid = $user->id;
             
