7365aaf0e7c6c0cf40fc7759394e67a16a8d2904
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-10-28 03:07:44 -0700

    ZenHub #638
    ZenHub #638

--- resources/views/hradmin/myorg/myorganization.blade.php
+++ resources/views/hradmin/myorg/myorganization.blade.php
@@ -40,7 +40,6 @@
         
     @endpush
     @push('js')
-        {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>   --}}
         {{-- <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script> --}}
         {{-- <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script> --}}
 
