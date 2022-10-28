baaecdec24b929b901e9c349c8ae7fecad51102c
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-10-28 13:55:07 -0700

    ZenHub #826
    ZenHub #826

--- app/Models/UserDemoJrView.php
+++ app/Models/UserDemoJrView.php
@@ -8,5 +8,21 @@ class UserDemoJrView extends Model {
 
     protected $table = 'user_demo_jr_view';
 
+    // protected $appends = [
+    //     'activeGoals',
+    //     'reportees',
+    //     'is_shared',
+    // ];
+
+    // public function activeGoals()
+    // {
+    //     // return $this->hasMany('App\Models\Goal')->where('status', 'active');
+    //     return 0;
+    // }
+
+    // public function reportees() {
+    //     // return $this->hasMany('App\Models\User', 'reporting_to');
+    //     return '';
+    // }
     
 }
