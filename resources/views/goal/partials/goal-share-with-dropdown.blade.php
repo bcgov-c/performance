f6b885d64c3d32655bd7541a988df07eef205ae3
jizhaogit <ji.zhao@telus.com>
2022-12-06 12:16:39 -0500

    #743 My Goals > Current Goals - Design 2.0

--- resources/views/goal/partials/goal-share-with-dropdown.blade.php
+++ resources/views/goal/partials/goal-share-with-dropdown.blade.php
@@ -4,12 +4,13 @@
         data-trigger="click" data-toggle="popover" data-placement="left" data-html="true" data-content="By default, all of your goals are private. Use the &quot;Share with&quot; dropdown menu to make a goal visible to selected employees. This lets team members know what you are working on and may help team members set their own goals." 
     @endif
     > -->   
-            
+    @if(!(isset($noLabel)) || (isset($noLabel) && $noLabel == false))        
     {{ $shareWithLabel ?? "Shared with"}}:&nbsp; 
     @if(!(isset($doNotShowInfo) && $doNotShowInfo))
         <i class="fa fa-info-circle" data-trigger="click" data-toggle="popover" data-placement="right" data-html="true" data-content="By default, all of your goals are private. Use the &quot;Share with&quot; dropdown menu to make a goal visible to selected employees. This lets team members know what you are working on and may help team members set their own goals." >
         </i>        
     @endif    
+    @endif
          
     <select multiple class="form-control search-users ml-1" id="search-users-{{$goal->id}}" name="share_with[{{$goal->id}}][]" data-goal-id="{{$goal->id}}">
         @php
