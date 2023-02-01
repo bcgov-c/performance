a19eac47b1ecee28815a8910c8e5d4a3f335f8df
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-02-01 14:04:29 -0800

    ZenHub #872
    ZenHub #872

--- app/Console/Commands/SetNextLevelManager.php
+++ app/Console/Commands/SetNextLevelManager.php
@@ -4,9 +4,11 @@ namespace App\Console\Commands;
 
 use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Position;
 use App\Models\EmployeeDemo;
 use App\Models\ExcusedReason;
 use App\Models\JobSchedAudit;
+use App\Models\JobDataAudit;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -74,24 +76,61 @@ class SetNextLevelManager extends Command
         ->whereNotNull('employee_demo.guid')
         ->orderBy('employee_demo.employee_id')
         ->orderBy('employee_demo.empl_record')
-        ->chunk(1000, function($employeeDemo) use (&$counter, &$updatecounter, $start_time) {
+        ->chunk(10000, function($employeeDemo) use (&$counter, &$updatecounter, $start_time, $audit_id) {
             foreach ($employeeDemo as $demo) {
                 $reporting_to = $this->getReportingUserId($demo);  
-                $user = User::where('guid', $demo->guid)->first();
+                $user = User::whereRaw("employee_id = '".$demo->employee_id."'")->first();
                 if ($user) {
                     if ($user->reporting_to != $reporting_to) {
-                        $user->reporting_to = $reporting_to;
-                        $user->last_sync_at = $start_time;
-                        $user->save();             
-                        // Update Reporting Tos
-                        if ($reporting_to) {
-                            $user->reportingTos()->updateOrCreate([ 'reporting_to_id' => $reporting_to, ]);
+                        DB::beginTransaction();
+                        try {
+                            $user->reporting_to = $reporting_to;
+                            $user->last_sync_at = $start_time;
+                            $user->save();             
+                            $old_values = [ 
+                                'table' => 'users',                        
+                                'employee_id' => $user->employee_id, 
+                                'reporting_to' => $user->reporting_to, 
+                                'last_sync_at' => $user->last_sync_at
+                            ];
+                            $new_values = [ 
+                                'table' => 'users', 
+                                'employee_id' => $demo->employee_id, 
+                                'reporting_to' => $reporting_to, 
+                                'last_sync_at' => $start_time
+                            ];
+                            $audit = new JobDataAudit;
+                            $audit->job_sched_id = $audit_id;
+                            $audit->old_values = json_encode($old_values);
+                            $audit->new_values = json_encode($new_values);
+                            $audit->save();
+                            // Update Reporting Tos
+                            if ($reporting_to) {
+                                $user->reportingTos()->updateOrCreate([ 'reporting_to_id' => $reporting_to ]);
+                            }
+                            $old_values = [ 
+                                'table' => 'user_reporting_tos'                        
+                            ];
+                            $new_values = [ 
+                                'table' => 'user_reporting_tos', 
+                                'employee_id' => $demo->employee_id, 
+                                'reporting_to_id' => $reporting_to 
+                            ];
+                            $audit = new JobDataAudit;
+                            $audit->job_sched_id = $audit_id;
+                            $audit->old_values = json_encode($old_values);
+                            $audit->new_values = json_encode($new_values);
+                            $audit->save();
+                            DB::commit();
+                            $this->info('EID '.$demo->employee_id.' - '.$demo->employee_name.' updated Manager to UID '.$reporting_to.'.');
+                            $updatecounter += 1;
+                        } catch (Exception $e) {
+                            echo 'Unable to reporting_to for EID # '.$demo->employee_id.'.'; echo "\r\n";
+                            DB::rollback();
                         }
-                        $this->info('EID '.$demo->employee_id.' - '.$demo->employee_name.' updated Manager to UID '.$reporting_to.'.');
-                        $updatecounter += 1;
-                    }
+                        }
                 } else {
-                    $this->info('EID '.$demo->employee_id.' - '.$demo->employee_name.' not found by guid.');
+                    $this->info('EID '.$demo->employee_id.' - '.$demo->employee_name.' not found.');
                 }
                 $counter += 1;
                 echo 'Processed '.$counter.'.  Updated '.$updatecounter.'.'; echo "\r";
@@ -119,38 +158,67 @@ class SetNextLevelManager extends Command
     public function getReportingUserId($employee){
         if ($employee->supervisor_emplid) {
             $supervisor = EmployeeDemo::where('employee_id', $employee->supervisor_emplid)
-                ->orderBy('job_indicator', 'desc')
-                ->orderBy('empl_record')
-                ->first();
+            ->orderBy('job_indicator', 'desc')
+            ->orderBy('empl_record')
+            ->first();
             if ($supervisor) {
-                $user = User::where('guid', str_replace('-', '', $supervisor->guid))->first();
+                $user = User::whereRaw("employee_id = '".$supervisor->employee_id."'")->first();
                 if ($user) {
                     return $user->id;
                 } else {
-                    $text = 'Supervisor Not found - ' . $employee->supervisor_emplid . ' | employee -' .
+                    $text = 'Supervisor Not found Supv EID = ' . $employee->supervisor_emplid . ' | EID = ' .
                         $employee->employee_id;
                     $this->info( 'exception ' . $text );
                 }
+            } else {
+                $text = 'Supervisor not found Supv EID = ' . $employee->supervisor_emplid;
+                $this->info( 'exception ' . $text );
             }
         } else {
-            $employee2 = EmployeeDemo::where('position_number', $employee->supervisor_position_number)
+            $employee2 = Position::where('position_nbr', $employee->supervisor_position_number)
+                ->whereNull('date_deleted')
+                ->first();
+            if ($employee2) {
+                $supervisor = EmployeeDemo::where('position_number', $employee2->reports_to)
                 ->orderBy('job_indicator', 'desc')
                 ->orderBy('empl_record')
                 ->first();
-            if ($employee2) {
-                $supervisor = EmployeeDemo::where('employee_id', $employee2->supervisor_emplid)
-                    ->orderBy('job_indicator', 'desc')
-                    ->orderBy('empl_record')
-                    ->first();
                 if ($supervisor) {
-                    $user = User::where('guid', str_replace('-', '', $supervisor->guid))->first();
+                    $user = User::whereRaw("employee_id = '".$supervisor->employee_id."'")->first();
                     if ($user) {
                         return $user->id;
                     } else {
-                        $text = 'Supervisor Not found - ' . $employee2->supervisor_emplid . ' | employee2 -' .
-                            $employee2->employee_id;
+                        $text = 'Supervisor Not found Posn # ' . $employee2->reports_to;
                         $this->info( 'exception ' . $text );
                     }
+                } else {
+                    $text = 'Supervisor Not found for Posn # ' . $employee2->reports_to;
+                    $this->info( 'exception ' . $text );
+                }   
+            } else {
+                $employee3 = Position::where('position_nbr', $employee->position_number)
+                ->whereNull('date_deleted')
+                ->first();
+                if ($employee3) {
+                    $supervisor = EmployeeDemo::where('position_number', $employee3->reports_to)
+                    ->orderBy('job_indicator', 'desc')
+                    ->orderBy('empl_record')
+                    ->first();
+                    if ($supervisor) {
+                        $user = User::whereRaw("employee_id = '".$supervisor->employee_id."'")->first();
+                        if ($user) {
+                            return $user->id;
+                        } else {
+                            $text = 'Supervisor Not found Posn # ' . $employee3->reports_to;
+                            $this->info( 'exception ' . $text );
+                        }
+                    } else {
+                        $text = 'Supervisor Posn Not found for Posn # ' . $employee3->position_nbr;
+                        $this->info( 'exception ' . $text );
+                    }
+                } else {
+                    $text = 'Supervisor Not found for EID = ' . $employee->employee_id;
+                    $this->info( 'exception ' . $text );
                 }
             }
         }
