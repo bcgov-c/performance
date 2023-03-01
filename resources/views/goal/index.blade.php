36feb892ea5a90ea3747238deb9746e5d68a4414
jizhaogit <ji.zhao@telus.com>
2023-03-01 08:12:15 -0500

    Zenhub #915 Goal Description field in Create New Goal clears out when added Tags are edited

--- resources/views/goal/index.blade.php
+++ resources/views/goal/index.blade.php
@@ -480,7 +480,6 @@
                 var goal_title = $("#goal_title").val();
                 var tags = $('.tags').val(); 
                 if (goal_title != '' && tags != ''){
-                   CKEDITOR.instances['what'].setData(''); 
                    CKEDITOR.instances['what'].setReadOnly(false);
                    CKEDITOR.instances['measure_of_success'].setReadOnly(false);
                    $('#start_date').prop("readonly",false);
@@ -496,8 +495,7 @@
             $(".tags").change(function(){
                 var goal_title = $("#goal_title").val();
                 var tags = $('.tags').val(); 
-                if (goal_title != '' && tags != ''){
-                    CKEDITOR.instances['what'].setData(''); 
+                if (goal_title != '' && tags != ''){ 
                     CKEDITOR.instances['what'].setReadOnly(false);
                     CKEDITOR.instances['measure_of_success'].setReadOnly(false);
                     $('#start_date').prop("readonly",false);
