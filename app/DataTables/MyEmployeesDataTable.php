327b14552021f2cfb1dde6250c8064c323708555
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-03-14 14:46:39 -0700

    Revert "ZenHub #924"
    This reverts commit f5bf6a83eb44de0e293aa767c848b3e92aa79b9e.

--- app/DataTables/MyEmployeesDataTable.php
+++ app/DataTables/MyEmployeesDataTable.php
@@ -46,7 +46,7 @@ class MyEmployeesDataTable extends DataTable
                 return view('my-team.partials.link-to-profile', compact(['row', 'text', 'landingPage']));
             })->addColumn('nextConversationDue', function ($row) {
                 $jr = EmployeeDemoJunior::where('employee_id', $row->employee_id)->getQuery()->orderBy('id', 'desc')->first();
-                if ($jr && isset($jr->excused_type) && $jr->excused_type) {
+                if (isset($jr->excused_type) && $jr->excused_type) {
                     if ($jr->excused_type == 'A') {
                         $text = 'Paused';
                         $landingPage = 'conversation.templates';
@@ -58,7 +58,7 @@ class MyEmployeesDataTable extends DataTable
                     $landingPage = 'conversation.templates';
                     return view('my-team.partials.link-to-profile', compact(["row", "text", "landingPage"]));
                 }
-                if ($jr && isset($jr->next_conversation_date) && $jr->next_conversation_date) {
+                if (isset($jr->next_conversation_date) && $jr->next_conversation_date) {
                     if ($jr->next_conversation_date) {
                         $text = Carbon::parse($jr->next_conversation_date)->format('M d, Y');
                         $landingPage = 'conversation.templates';
