159f266db2cdcb979c592803ae9b226606bdff0c
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-04-05 12:36:30 -0700

    ZenHub #785 - Fix Migration
    ZenHub #785 - Fix Migration

--- database/migrations/2023_04_05_032747_alter_hr_user_demo_jr_view_4.php
+++ database/migrations/2023_04_05_032747_alter_hr_user_demo_jr_view_4.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterHrUserDemoJrView3 extends Migration
+class AlterHrUserDemoJrView4 extends Migration
 {
     /**
      * Run the migrations.
