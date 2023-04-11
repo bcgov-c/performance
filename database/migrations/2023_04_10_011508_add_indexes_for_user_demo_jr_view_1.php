1be5e57eb0768bb6cc2586f920b21cbe0f758576
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-04-10 18:01:04 -0700

    ZenHub #785 - New Index
    ZenHub #785 - New Index

---
+++ database/migrations/2023_04_10_011508_add_indexes_for_user_demo_jr_view_1.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddIndexesForUserDemoJrView1 extends Migration
+{
+    public function createTableIndexIfNotExist($tableName, $indexName, $fieldNames = []) {
+        $exists = DB::table('information_schema.statistics')
+            ->where('table_name', $tableName)
+            ->where('index_name', $indexName)
+            ->get()->count();
+        if (!$exists) {
+            Schema::table($tableName, function (Blueprint $table) use($indexName, $fieldNames) {
+                $table->index($fieldNames, $indexName);
+            });
+        }
+    }
+
+    public function dropExistingTableIndex($tableName, $indexName) {
+        $exists = DB::table('information_schema.statistics')
+            ->where('table_name', $tableName)
+            ->where('index_name', $indexName)
+            ->get()->count();
+        if ($exists) {
+            Schema::table($tableName, function($table) use($indexName) {
+                $table->dropIndex($indexName);
+            });
+        }
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->down();
+        $this->createTableIndexIfNotExist('employee_demo', 'employee_demo_deptid_index', ['deptid']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->dropExistingTableIndex('employee_demo', 'employee_demo_deptid_index');
+    }
+}
