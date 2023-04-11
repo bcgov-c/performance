35033ad46f5d6bf338f8c55e5cc1a171030d84ac
jizhaogit <ji.zhao@telus.com>
2023-04-11 11:40:52 -0400

    Zenhub #930 As an HR/Sys Admin, I want to see how many employees have open or completed conversations

--- app/Http/Controllers/SysAdmin/SysadminStatisticsReportController.php
+++ app/Http/Controllers/SysAdmin/SysadminStatisticsReportController.php
@@ -2347,7 +2347,8 @@ class SysadminStatisticsReportController extends Controller
                 );
         
                 $columns = ["Employee ID", "Employee Name", "Email",
-                                "Organization", "Level 1", "Level 2", "Level 3", "Level 4",
+                                "Organization","Next Conversation Due","Reporting To",
+                                "Level 1", "Level 2", "Level 3", "Level 4",
                            ];
         
                 $callback = function() use($users, $columns) {
@@ -2359,12 +2360,15 @@ class SysadminStatisticsReportController extends Controller
                         $row['Name'] = $user->employee_name;
                         $row['Email'] = $user->email;
                         $row['Organization'] = $user->organization;
+                        $row['next_conversation_date'] = $user->next_conversation_date;
+                        $row['reporting_to_name'] = $user->reporting_to_name;
                         $row['Level 1'] = $user->level1_program;
                         $row['Level 2'] = $user->level2_division;
                         $row['Level 3'] = $user->level3_branch;
                         $row['Level 4'] = $user->level4;
         
                         fputcsv($file, array($row['Employee ID'], $row['Name'], $row['Email'], $row['Organization'],
+                                    $row['next_conversation_date'],$row['reporting_to_name'],
                                     $row['Level 1'], $row['Level 2'], $row['Level 3'], $row['Level 4'] ));
                     }
         
@@ -2402,7 +2406,8 @@ class SysadminStatisticsReportController extends Controller
                 );
         
                 $columns = ["Employee ID", "Employee Name", "Email",
-                                "Organization", "Level 1", "Level 2", "Level 3", "Level 4",
+                                "Organization","Next Conversation Due","Reporting To",
+                                "Level 1", "Level 2", "Level 3", "Level 4",
                            ];
         
                 $callback = function() use($users, $columns) {
@@ -2414,12 +2419,15 @@ class SysadminStatisticsReportController extends Controller
                         $row['Name'] = $user->employee_name;
                         $row['Email'] = $user->email;
                         $row['Organization'] = $user->organization;
+                        $row['next_conversation_date'] = $user->next_conversation_date;
+                        $row['reporting_to_name'] = $user->reporting_to_name;
                         $row['Level 1'] = $user->level1_program;
                         $row['Level 2'] = $user->level2_division;
                         $row['Level 3'] = $user->level3_branch;
                         $row['Level 4'] = $user->level4;
         
                         fputcsv($file, array($row['Employee ID'], $row['Name'], $row['Email'], $row['Organization'],
+                                    $row['next_conversation_date'],$row['reporting_to_name'],
                                     $row['Level 1'], $row['Level 2'], $row['Level 3'], $row['Level 4'] ));
                     }
         
