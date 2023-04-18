dd52fe709912d60288d2c77ac055a4fe65c807d1
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-04-18 15:38:17 -0700

    ZebHub #785 - Performance Improvement for HR Admin Access
    ZebHub #785 - Performance Improvement for HR Admin Access

--- database/migrations/2023_04_18_120437_alter_hr_user_demo_jr_view_12.php
+++ database/migrations/2023_04_18_120437_alter_hr_user_demo_jr_view_12.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterHrUserDemoJrView11 extends Migration
+class AlterHrUserDemoJrView12 extends Migration
 {
     /**
      * Run the migrations.
