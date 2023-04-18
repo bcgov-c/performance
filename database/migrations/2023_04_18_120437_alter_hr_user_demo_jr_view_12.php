03934661ad2d980451986629b27927b19fe26cf1
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-04-18 14:56:34 -0700

    Test

--- database/migrations/2023_04_18_120437_alter_hr_user_demo_jr_view_12.php
+++ database/migrations/2023_04_18_120437_alter_hr_user_demo_jr_view_12.php
@@ -96,7 +96,6 @@ class AlterHrUserDemoJrView12 extends Migration
             FROM 
 				auth_users 
 					AS au
-					USE INDEX (idx_auth_users_type_auth_id_user_id)
 				INNER JOIN users 
 					AS u 
 					USE INDEX (idx_users_id)
