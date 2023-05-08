220d1d03b256853aca27303e799889dc8fbe8c97
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-05-08 11:57:28 -0700

    ZenHub #941 - Verbage Changes
    ZenHub #941 - Verbage Changes

--- resources/views/my-team/partials/employee-profile-sharing-modal.blade.php
+++ resources/views/my-team/partials/employee-profile-sharing-modal.blade.php
@@ -14,9 +14,9 @@
                 <strong>The profile of <span class="user-name"></span> is currently being shared with the following users:</strong> <br>
                     <div class="shared-with-list">None</div>
                 <strong>Share this profile with another user</strong>
-                <p>Supervisors and administrators may share all or part of an employee's PDP profile with another supervisor or staff for a legitimate business reason. The full profile or the Conversations section should only be shared with people who normally handle employees' permanent personnel records (i.e. Public Service Agency or co-supervisors). The Goals section can be shared more broadly (i.e. with project team leads) to encourage collaboration and feedback on specific goals.</p>
-                <p>An employee may also wish to share their profile with someone other than a direct supervisor (for example, a hiring manager). In order to do this - the employee's consent is required.</p>
-                <p>To continue, please use the functions below to select the employee profiles that you would like to share, the person you would like to share the profiles with, which elements you would like to share, and your reason for sharing the profile.</p>
+                <p>Supervisors and administrators may share an employee's PDP profile with another supervisor or staff for a legitimate business reason. The profile should only be shared with people who normally handle employees' permanent personnel records (i.e. Public Service Agency or co-supervisors). An employee may also wish to share their profile with someone other than a direct supervisor (for example, a hiring manager). In order to do this - the employee's consent is required.</p>
+                <!-- <p>An employee may also wish to share their profile with someone other than a direct supervisor (for example, a hiring manager). In order to do this - the employee's consent is required.</p>
+                <p>To continue, please use the functions below to select the employee profiles that you would like to share, the person you would like to share the profiles with, which elements you would like to share, and your reason for sharing the profile.</p> -->
                 <form id="share-profile-form" action="{{ route('my-team.share-profile') }}" method="POST" onsubmit="confirm('Are you sure you want to share the selected profile(s)?')">
                     @csrf
                     <input type="hidden" name="shared_id">
@@ -34,7 +34,7 @@
                         <div class="col-6">
                             <!-- <x-input name="reason" label="Reason" tooltip="Reason tooltip"></x-input> -->
                             <b>Reason for sharing</b>
-                            <i class="fa fa-info-circle" name="reason" data-trigger='click' data-toggle="popover" data-placement="right" data-html="true" data-content="Provide a brief explanation of why the profile elements are being shared. For example: <br><br><ul><li> Sharing profile with co-supervisor </li><li>Sharing goals section with project team lead</li><li>Sharing conversations section with hiring manager per employee request</li></ul>"> </i> 
+                            <i class="fa fa-info-circle" name="reason" data-trigger='click' data-toggle="popover" data-placement="right" data-html="true" data-content="Provide a brief explanation of why the profile is being shared. For example: <br><br><ul><li> Sharing profile with co-supervisor </li><li>Sharing profile because of inaccurate data in PeopleSoft</li><li>Sharing with hiring manager per employee request</li></ul>"> </i> 
                             <x-input name="reason"></x-input>
                         </div>
                     </div>
