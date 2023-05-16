6bab3e35a3055331fd64308ed910a95e6f92a803
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-05-16 10:39:17 -0700

    ZenHub #966 - Resources Changes
    ZenHub #966 - Resources Changes

--- routes/resource.php
+++ routes/resource.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('resource/user-guide', [ResourceController::class, 'userguide'])->name('resource.user-guide');
-Route::get('resource/goal-setting', [ResourceController::class, 'goalsetting'])->name('resource.goal-setting');
-Route::get('resource/conversations', [ResourceController::class, 'conversations'])->name('resource.conversations');
-Route::get('resource/contact', [ResourceController::class, 'contact'])->name('resource.contact');
+// Route::get('resource/user-guide', [ResourceController::class, 'userguide'])->name('resource.user-guide');
+// Route::get('resource/goal-setting', [ResourceController::class, 'goalsetting'])->name('resource.goal-setting');
+// Route::get('resource/conversations', [ResourceController::class, 'conversations'])->name('resource.conversations');
+// Route::get('resource/contact', [ResourceController::class, 'contact'])->name('resource.contact');
+Route::get('resources', [ResourceController::class, 'userguide'])->name('resource.user-guide');
+Route::get('resources/goal-setting', [ResourceController::class, 'goalsetting'])->name('resource.goal-setting');
+Route::get('resources/conversations', [ResourceController::class, 'conversations'])->name('resource.conversations');
+Route::get('resources/contact', [ResourceController::class, 'contact'])->name('resource.contact');
