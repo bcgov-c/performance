fa95a86ac5af2e8e733b344c7daa8805415adc07
jizhaogit <ji.zhao@telus.com>
2023-05-18 20:38:05 -0400

    Zenhub #620

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -459,6 +459,7 @@ class GoalController extends Controller
             ->leftjoin('goal_types', 'goal_types.id', 'goals.goal_type_id')   
             ->whereIn('goals.by_admin', [1, 2])
             ->where('goals.is_library', true)
+            ->whereNull('goals.deleted_at')        
             ->groupBy('goals.id', 'goals.title', 'goals.goal_type_id', 'goals.created_at', 'goals.user_id', 'u2.name', 'goals.is_mandatory');
         // Admin List filter below
         if ($request->has('goal_bank_mandatory') && $request->goal_bank_mandatory !== null) {
@@ -501,7 +502,11 @@ class GoalController extends Controller
         }
         if ($request->has('goal_bank_createdby') && $request->goal_bank_createdby) {
             // $query = $query->where('user_id', $request->created_by);
-            $adminGoals = $adminGoals->where('created_by', $request->goal_bank_createdby);
+            if(is_numeric($request->goal_bank_createdby)) {
+                $adminGoals = $adminGoals->where('created_by', $request->goal_bank_createdby);
+            } else {
+                $adminGoals = $adminGoals->where('display_name', 'like',$request->goal_bank_createdby);
+            }
         }
 
         $adminGoalsInherited = Goal::withoutGlobalScopes()
@@ -516,6 +521,7 @@ class GoalController extends Controller
             ->leftjoin('goal_types', 'goal_types.id', 'goals.goal_type_id')   
             ->whereIn('goals.by_admin', [1, 2])
             ->where('goals.is_library', true)
+            ->whereNull('goals.deleted_at')        
             ->where(function ($where) use ($authId) {
                 return $where->whereRaw("
                         (
@@ -569,13 +575,18 @@ class GoalController extends Controller
         }
         if ($request->has('goal_bank_createdby') && $request->goal_bank_createdby) {
             // $query = $query->where('user_id', $request->created_by);
-            $adminGoalsInherited = $adminGoalsInherited->where('created_by', $request->goal_bank_createdby);
+            if(is_numeric($request->goal_bank_createdby)) {
+                $adminGoalsInherited = $adminGoalsInherited->where('created_by', $request->goal_bank_createdby);
+            } else {
+                $adminGoalsInherited = $adminGoalsInherited->where('display_name', 'like',$request->goal_bank_createdby);
+            }
         }
 
         // $adminGoals = $adminGoals->union($adminGoalsInherited);
 
         $query = Goal::withoutGlobalScope(NonLibraryScope::class)
         ->where('is_library', true)
+        ->whereNull('goals.deleted_at')        
         ->join('users', 'goals.user_id', '=', 'users.id')          
         ->leftjoin('users as u2', 'u2.id', '=', 'goals.created_by')
         ->leftjoin('goal_types', 'goal_types.id', '=', 'goals.goal_type_id')    
@@ -627,7 +638,11 @@ class GoalController extends Controller
 
         if ($request->has('goal_bank_createdby') && $request->goal_bank_createdby) {
             // $query = $query->where('user_id', $request->created_by);
-            $query = $query->where('created_by', $request->goal_bank_createdby);
+            if(is_numeric($request->goal_bank_createdby)) {
+                $query = $query->where('created_by', $request->goal_bank_createdby);
+            } else {
+                $query = $query->where('display_name', 'like',$request->goal_bank_createdby);
+            }
         }
 
         $query->whereHas('sharedWith', function($query) {
@@ -855,12 +870,13 @@ class GoalController extends Controller
             ]
         ];
         $createdBy = Goal::withoutGlobalScope(NonLibraryScope::class)
-            ->where('is_library', true)
+            //->where('is_library', true)
             ->whereHas('sharedWith', function($query) {
                 $query->where('user_id', Auth::id());
             })
             ->with('user')
-            ->whereNull('display_name')        
+            ->whereNull('display_name')
+            ->whereNull('deleted_at')        
             ->groupBy('user_id')
             ->get()
             ->pluck('user')
@@ -868,6 +884,9 @@ class GoalController extends Controller
        
         $display_names = DB::table('goals')
                     ->select('display_name')
+                    ->join('goals_shared_with', 'goals_shared_with.goal_id', 'goals.id')
+                    ->where('goals_shared_with.user_id', Auth::id())
+                    ->whereNull('deleted_at')
                     ->distinct()
                     ->pluck('display_name')
                     ->toArray();
