7db5610651121b5f28a6264c1dcc91ba7922017f
JonTelus <jonathan.wood@telus.com>
2023-05-30 09:54:20 -0700

    #940 Resource page changes
    Update user guide to reflect new functions
    Add FAQ tab
    Update Goal Setting and Performance Conversations if required

---
+++ resources/views/resource/partials/faq/2.blade.php
@@ -0,0 +1,72 @@
+<h5>a. How do I access past MyPerformance files?</h5>
+<p>You will not lose access to your past MyPerformance files. The MyPerformance system will remain online during the transition to performance development and the PDP. Once that transition is complete, the MyPerformance files will be made available to you in a secure archive. You cannot copy MyPerformance profiles directly into the PDP, though you can copy and paste any relevant text into the new format.</p>
+<p>Sign-off by both parties in MyPerformance is required for the profile to be archived and made available. </p>
+<br>
+<h5>b. Who can assist my employees if they need help with the PDP?</h5>
+<p>Supervisors are supposed to help employees with their PDP profiles whenever possible. All supervisors and employees are encouraged to review the <a href="https://performance.gov.bc.ca/resources">Resources Section</a> of the PDP for a helpful user guide, tutorial videos, and best practice tips for setting effective goals and having effective conversations.</p>
+<p>If you can&rsquo;t find the answer after reviewing this material, you can escalate your request to your ministry contact listed in the <a href="https://performance.gov.bc.ca/resources/contact">Contacts Section</a> of the PDP.</p>
+<p>If you are experiencing a technical issue, first check to see if the answer is in the PDP <a href="https://performance.gov.bc.ca/resources">User Guide</a>. If you cannot find the answer, we can forward your email to someone who can investigate.</p>
+<br>
+<h5>c. How do I add a goal to my PDP profile?</h5>
+<p>There are two ways to include a goal in your PDP profile. You can:</p>
+<ul>
+    <li>Create a new goal using the “Create New Goal” function; or</li>
+    <li>Import a goal that has been assigned to you by your supervisor or organization using the “Add Goal from Goal Bank” function.</li>
+</ul>
+<p>When you launch the “Create New Goal” screen, you will be asked to select a goal type, goal title, and goal tags to help you identify and sort your goals. All three sections are mandatory. You can access more detailed information on these sections by clicking on the information “i” icon next to each heading in the app. There are tutorial videos and best practice tips to set effective goals available in the <a href="https://performance.gov.bc.ca/resources">PDP Resources Section</a>.</p>
+<p>The second way to include a goal in your PDP profile is to import a goal from your Goal Bank. This is a list of goals created for you by your supervisor or organization. Some goals will be marked as mandatory, but most are suggested starting points for you to consider. You can click on a goal to view the details and add it to your own profile. If needed, you can edit the goal to personalize it once it is in your profile.</p>
+<br>
+<h5>d. How do I share a goal with a colleague?</h5>
+<p>Sharing a goal with a colleague is easy to do. Navigate to your My Current Goals page and locate the goal that you want to share. Click on the drop-down list under the “Shared With” heading, type in the name of your colleague, and save your selection. You can repeat this process to add additional colleagues to the goal.</p>
+<p>The colleagues you selected will now see your goal on the tab “Goals Shared with Me”. They will be able to access the goal and add comments. </p>
+<p>If needed, you can remove a colleague from your goal by going back to the “Shared With” drop-down and clicking “x” next to their name.</p>
+<p>The person that originally created the goal will be able to change the goal status and mark it as achieved or archived on behalf of the group. This will move the goal to the Past Goals tab for all users with access to the goal.</p>
+<br>
+<h5>e. How do I set up a performance conversation with my employee / supervisor?</h5>
+<p>The My Conversations section is designed to support employees and supervisors to have the right conversation at the right time. The topic of each performance conversation does not have to be the same. The PDP has templates to support conversations focused on onboarding a new employee, goal setting, performance check-ins, career development, and performance improvement. Each of these count as a performance conversation.</p>
+<p>To request a conversation with your employee / supervisor, navigate to My Conversations &#62; Conversation Templates. Select the template that best matches the conversation you&rsquo;d like to have, select the appropriate person from the participants drop-down menu, and click “Start Conversation”. This will send a notification to the other person that you would like to meet and will create a conversation file on your “Open Conversations” tab under either the “Conversations with my Supervisor” or “Conversations with my Team” heading. </p>
+<p>Note that the conversation will still need to be scheduled independently in your Outlook calendar at a time that works for both you and your supervisor. There is no direct connection between PDP and Outlook calendar.</p>
+<br>
+<h5>f. I am a supervisor. How do I create a goal in my team&rsquo;s Goal Bank?</h5>
+<p>Navigate to My Goals &#62; Goal Bank &#62; Team Goal Bank and click the button to “Add Goal to Bank”. You create a goal here just as you would for yourself and then select team members to push the goal out to in the “Audience” drop-down. You can include yourself in the audience if you&rsquo;d also like to see the goal appear in your own goal bank.</p>
+<p>This option creates a separate copy of the goal for each user that copies it from the goal bank. This means that you can add different comments to each team member&rsquo;s version of the goal, and the goal status can be changed to achieved or archived for each team member individually without impacting the other users.</p>
+<p>You can remove a goal from your team&rsquo;s goal bank by navigating to My Goals &#62; Goal Bank &#62; Team Goal Bank and deleting the entry. This will remove it from all team members Goal Banks as well. Note that, if someone has already copied the goal into their own profile, they will keep their version of the goal even if you delete the original from the Goal Bank.</p>
+<br>
+<h5>g. I&rsquo;m a supervisor. I want to view my employee&rsquo;s goals and upcoming conversation deadlines. How do I do this?</h5>
+<p>As a supervisor, you have access to a My Team page that has info about your direct and shared reports. Here you can find a summary view of how many active goals each employee has, when their next performance conversation is due, and whether or not they have been shared or excused in the PDP.</p>
+<p>Please note that the next conversation due date is calculated by the PDP as part of an overnight background process. That means that an employee&rsquo;s due date will not be updated to reflect a signed-off conversation until the day after sign-off is complete.</p>
+<p>Clicking on an employee&rsquo;s name will take you to their profile where you can view the details of their goals and conversations. You can add comments to their goals or any open conversations right in their profile.</p>
+<p>You will see a banner across the top of the page letting you know when you are viewing an employee&rsquo;s profile. To return to your own profile at any time, click the “Return to my profile” button in the top right corner. Clicking “back” in your web browser could cause log-in issues, so please use the “Return to my profile” option instead.</p>
+<br>
+<h5>h. Is there an autosave function on the application?</h5>
+<p>Yes, currently 20 minutes. After 20 minutes of not saving your work, the PDP will autosave any open windows and launch a pop-up window to let you know what happened and why.</p>
+<br>
+<h5>i. Is there a timeout on the application?</h5>
+<p>Yes, currently 120 minutes. This timeout supports the corporate IM/IT Security Policy. To get back into PDP, simply click on the link to return to the application.</p>
+<br>
+<h5>j. Can I access the PDP from home?</h5>
+<p>Yes, you just need to have the tool&rsquo;s website URL. You can type that into the browser and you will be prompted for your IDIR login information.</p>
+<br>
+<h5>k. I am a member of my Ministry&rsquo;s Strategic HR office and I need to have Administrator access. How do I receive this status?</h5>
+<p>Please submit a request for PDP Administrator Access through AskMyHR, choose "I am submitting this request as or on behalf of &#45; Myself" and select the "HR Software Systems Support>Performance Development Platform" category. You will need to include a completed Request for PDP Admin Access form. Requests will be forwarded to the application analyst and you will be notified as soon as possible.</p>
+<p>Once you have this access you can do the following:</p>
+<ul>
+    <li>View the employee list for your organization (including reporting relationships)</li>
+    <li>Run reports for your ministry</li>
+    <li>Excuse and share employees</li>
+    <li>Create goals for your organization&rsquo;s goal bank</li>
+</ul>
+<br>
+<h5>l. What happens when an employee clicks “I disagree with the information contained in this performance review”?</h5>
+<p>The supervisor will be notified by email and a note will be added to their PDP homepage. The disagreement is registered in the system and will be visible when a report is run by the employee&rsquo;s ministry or PSA. In addition to clicking the “disagree” option, the employee must still sign-off the conversation using their employee ID to complete the process.</p>
+<br>
+<h5>m. My employee clicked “I disagree with the information contained in this performance review”. What do I do?</h5>
+<p>When an employee disagrees with the supervisor&rsquo;s comments and/or assessment, the employee has the option to check off this box when signing off on the conversation.</p>
+<p>This is another opportunity for the supervisor and employee to discuss the employee&rsquo;s perspective so that the supervisor can seek to understand and consider the employee&rsquo;s concerns and reasons for disagreeing with the conversation.</p>
+<p>The Conversations Resources area of the PDP can support you in this discussion. You may find the sections on “Asking for Feedback/Inquiring Into the Other&rsquo;s Perspective” and “Addressing a Performance Issue” particularly helpful.</p>
+<p>If you would like to speak with someone for guidance and advice, we could forward your Service Request to an HR Specialist. You may also want to partner with a Coach by signing up for individual Coaching Services. Please visit MyHR for more information on <a href="https://psa.frontlinesvc.com/app/answers/intermediate/a_id/1950">Coaching</a> and requesting Coaching.</p>
+
+
+
+
+
