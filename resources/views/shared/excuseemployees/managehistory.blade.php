082c968371f9bbfd96113e8a68685aeffb5829d0
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-06-07 07:39:04 -0700

    Revert "ZenHub #785 - Fix Excused Loading"
    This reverts commit 5202dd5f01d609fb52a41c0579cc8fd117698582.

--- resources/views/shared/excuseemployees/managehistory.blade.php
+++ resources/views/shared/excuseemployees/managehistory.blade.php
@@ -81,7 +81,7 @@
                         {title: 'ID', ariaTitle: 'ID', target: 0, type: 'string', data: 'employee_id', name: 'u.employee_id', searchable: true, className: 'dt-nowrap show-modal'},
                         {title: 'Name', ariaTitle: 'Name', target: 0, type: 'string', data: 'employee_name', name: 'u.employee_name', searchable: true, className: 'dt-nowrap show-modal'},
                         {title: 'Classification', ariaTitle: 'Classification', target: 0, type: 'string', data: 'jobcode_desc', name: 'u.jobcode_desc', searchable: true, className: 'dt-nowrap show-modal'},
-                        {title: 'Excused', ariaTitle: 'Excused', target: 0, type: 'string', data: 'j_excusedtype', name: 'j_excusedtype', searchable: false, visible: true, className: 'dt-nowrap show-modal'},
+                        {title: 'Excused', ariaTitle: 'Excused', target: 0, type: 'string', data: 'j_excusedtype', name: 'u.j_excusedtype', searchable: false, visible: true, className: 'dt-nowrap show-modal'},
                         {title: 'Excused Reason', ariaTitle: 'Excused Reason', target: 0, type: 'string', data: 'j_excused_reason_desc', name: 'u.j_excused_reason_desc', searchable: false, visible: true, className: 'dt-nowrap show-modal'},
                         {title: 'Excused By', ariaTitle: 'Excused By', target: 0, type: 'string', data: 'excused_by_name', name: 'u.excused_by_name', searchable: false, visible: true, className: 'dt-nowrap show-modal'},
                         {title: 'Start Date', ariaTitle: 'Start Date', target: 0, type: 'string', data: 'startdate_string', name: 'startdate_string', searchable: false, visible: true, className: 'dt-nowrap show-modal'},
