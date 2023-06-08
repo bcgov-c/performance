d3f4db4ac52296a2269799fc55b0f3b98e698acc
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-06-08 03:00:17 -0700

    ZenHub #796 - Fix New Org Tree Scenario
    ZenHub #796 - Fix New Org Tree Scenario

--- app/Console/Commands/BuildEmployeeDemoTree.php
+++ app/Console/Commands/BuildEmployeeDemoTree.php
@@ -3,10 +3,12 @@
 namespace App\Console\Commands;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use App\Models\EmployeeDemoTree;
+use App\Models\EmployeeDemoTreeTemp;
 use App\Models\OrganizationHierarchy;
 use App\Models\JobSchedAudit;
 use App\Models\EmployeeDemo;
@@ -67,6 +69,7 @@ class BuildEmployeeDemoTree extends Command
         $total = 0;
 
         if ($switch == 'on' || $manualoverride) {
+            EmployeeDemoTreeTemp::truncate();
             $level = 0;
             do {
                 $this->info(Carbon::now()->format('c')." - Processing Level {$level}...");
@@ -94,115 +97,80 @@ class BuildEmployeeDemoTree extends Command
                 }
                 $allDepts = $allDepts->get();
                 foreach ($allDepts AS $dept) {
-                    $node = EmployeeDemoTree::where('id', $dept->okey)->first();
                     $groupcount = EmployeeDemo::join('ods_dept_org_hierarchy', 'employee_demo.deptid', 'ods_dept_org_hierarchy.deptid')
                         ->whereNull('employee_demo.date_deleted')
                         ->whereRaw("ods_dept_org_hierarchy.{$field} = {$dept->okey}")
                         ->count();
-                    if (!$node) {
-                        $node = new EmployeeDemoTree([
-                            'id' => $dept->okey,
-                            'name' => $dept->name,
-                            'deptid' => $dept->deptid,
-                            'status' => 1,
-                            'level' => $dept->ulevel,
-                            'headcount' => $dept->headcount,
-                            'groupcount' => $groupcount,
-                            'organization' =>  $dept->organization_label,
-                            'level1_program' =>  $dept->level1_label,
-                            'level2_division' =>  $dept->level2_label,
-                            'level3_branch' =>  $dept->level3_label,
-                            'level4' =>  $dept->level4_label,
-                            'level5' =>  $dept->level5_label,
-                            'organization_key' =>  $dept->organization_key,
-                            'level1_key' =>  $dept->level1_key,
-                            'level2_key' =>  $dept->level2_key,
-                            'level3_key' =>  $dept->level3_key,
-                            'level4_key' =>  $dept->level4_key,
-                            'level5_key' =>  $dept->level5_key,
-                            'organization_deptid' =>  $dept->organization_deptid,
-                            'level1_deptid' =>  $dept->level1_deptid,
-                            'level2_deptid' =>  $dept->level2_deptid,
-                            'level3_deptid' =>  $dept->level3_deptid,
-                            'level4_deptid' =>  $dept->level4_deptid,
-                            'level5_deptid' =>  $dept->level5_deptid,
-                            'organization_orgid' =>  $dept->organization_orgid,
-                            'level1_orgid' =>  $dept->level1_orgid,
-                            'level2_orgid' =>  $dept->level2_orgid,
-                            'level3_orgid' =>  $dept->level3_orgid,
-                            'level4_orgid' =>  $dept->level4_orgid,
-                            'level5_orgid' =>  $dept->level5_orgid,
-                        ]);
-                        switch ($level) {
-                            case 0:
-                                $node->saveAsRoot(); 
-                                break;
-                            case 1:
-                                $node->parent_id = $dept->organization_key;     
-                                $node->save();
-                                break;              
-                            case 2:
-                            case 3:
-                            case 4:
-                            case 5:
-                                $level2 = $level - 1;
-                                $field = "level{$level2}_key";
-                                if ($dept->okey != $dept->{$field}) {
-                                    $node->parent_id = $dept->{$field};     
-                                }
-                                $node->save();
-                                break;              
-                            default:
-                                break;
-                        }
-                        $this->info(Carbon::now()->format('c')." -   created {$level} - {$dept->okey} - {$dept->name}");
-                        $count_insert++;
-                    } else {
-                        $node->name = $dept->name;
-                        $node->deptid = $dept->deptid;
-                        $node->level = $dept->ulevel;
-                        $node->headcount = $dept->headcount;
-                        $node->groupcount = $groupcount;
-                        $node->organization = $dept->organization_label;
-                        $node->level1_program = $dept->level1_label;
-                        $node->level2_division = $dept->level2_label;
-                        $node->level3_branch= $dept->level3_label;
-                        $node->level4 = $dept->level4_label;
-                        $node->level5 = $dept->level5_label;
-                        $node->organization_deptid = $dept->organization_deptid;
-                        $node->level1_deptid = $dept->level1_deptid;
-                        $node->level2_deptid = $dept->level2_deptid;
-                        $node->level3_deptid = $dept->level3_deptid;
-                        $node->level4_deptid = $dept->level4_deptid;
-                        $node->level5_deptid = $dept->level5_deptid;
-                        switch ($level) {
-                            case 0:
-                                $node->parent_id = null;     
-                                break;
-                            case 1:
-                                $node->parent_id = $dept->organization_key;     
-                                break;              
-                            case 2:
-                            case 3:
-                            case 4:
-                            case 5:
-                                $level2 = $level - 1;
-                                $field = "level{$level2}_key";
-                                if ($dept->okey != $dept->{$field}) {
-                                    $node->parent_id = $dept->{$field};     
-                                }
-                                break;              
-                            default:
-                                break;
-                        }
-                        $node->save();
-                        $this->info(Carbon::now()->format('c')." -   updated {$level} - {$dept->okey} - {$dept->name}");
-                        $count_update++;
+                    $node = new EmployeeDemoTreeTemp([
+                        'id' => $dept->okey,
+                        'name' => $dept->name,
+                        'deptid' => $dept->deptid,
+                        'status' => 1,
+                        'level' => $dept->ulevel,
+                        'headcount' => $dept->headcount,
+                        'groupcount' => $groupcount,
+                        'organization' =>  $dept->organization_label,
+                        'level1_program' =>  $dept->level1_label,
+                        'level2_division' =>  $dept->level2_label,
+                        'level3_branch' =>  $dept->level3_label,
+                        'level4' =>  $dept->level4_label,
+                        'level5' =>  $dept->level5_label,
+                        'organization_key' =>  $dept->organization_key,
+                        'level1_key' =>  $dept->level1_key,
+                        'level2_key' =>  $dept->level2_key,
+                        'level3_key' =>  $dept->level3_key,
+                        'level4_key' =>  $dept->level4_key,
+                        'level5_key' =>  $dept->level5_key,
+                        'organization_deptid' =>  $dept->organization_deptid,
+                        'level1_deptid' =>  $dept->level1_deptid,
+                        'level2_deptid' =>  $dept->level2_deptid,
+                        'level3_deptid' =>  $dept->level3_deptid,
+                        'level4_deptid' =>  $dept->level4_deptid,
+                        'level5_deptid' =>  $dept->level5_deptid,
+                        'organization_orgid' =>  $dept->organization_orgid,
+                        'level1_orgid' =>  $dept->level1_orgid,
+                        'level2_orgid' =>  $dept->level2_orgid,
+                        'level3_orgid' =>  $dept->level3_orgid,
+                        'level4_orgid' =>  $dept->level4_orgid,
+                        'level5_orgid' =>  $dept->level5_orgid,
+                    ]);
+                    switch ($level) {
+                        case 0:
+                            $node->saveAsRoot(); 
+                            break;
+                        case 1:
+                            $node->parent_id = $dept->organization_key;     
+                            $node->save();
+                            break;              
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                            $level2 = $level - 1;
+                            $field = "level{$level2}_key";
+                            if ($dept->okey != $dept->{$field}) {
+                                $node->parent_id = $dept->{$field};     
+                            }
+                            $node->save();
+                            break;              
+                        default:
+                            break;
                     }
+                    $this->info(Carbon::now()->format('c')." -   created {$level} - {$dept->okey} - {$dept->name}");
                     $total++;
                 }
                 $level++;
             } while ($level < 6);
+
+            // Move new tree
+            if($total > 0) {
+                $this->info(Carbon::now()->format('c').' - Copying tree from temp...');
+                EmployeeDemoTree::truncate();
+                \DB::statement("INSERT INTO employee_demo_tree SELECT * FROM employee_demo_tree_temp");
+                $this->info(Carbon::now()->format('c').' - Copy completed.');
+            } else {
+                $this->info(Carbon::now()->format('c').' - No new tree generated. Keeping previous tree.');
+            }
 
             // Update OrgId in employee_demo table
             $this->info(Carbon::now()->format('c').' - Updating Org Ids in employee_demo...');
@@ -219,7 +187,7 @@ class BuildEmployeeDemoTree extends Command
                     'start_time' => date('Y-m-d H:i:s', strtotime($start_time)),
                     'end_time' => date('Y-m-d H:i:s', strtotime($end_time)),
                     'status' => 'Completed',
-                    'details' => 'Processed '.$total.' rows. Inserted '.$count_insert.' rows. Updated '.$count_update.' rows. Deleted '.$count_delete.' rows. ',
+                    'details' => 'Processed '.$total.' rows.',
                 ]
             );
 
