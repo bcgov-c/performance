d5b861ca246dfb6355b3daf4090185cc21f31040
jizhaogit <ji.zhao@telus.com>
2023-06-12 09:38:25 -0400

    Zenhub #898 Statistics and Reporting - Update to Conversation Summary Chart
    Zenhub #898
    
    Zenhub #898 Statistics and Reporting - Update to Conversation Summary Chart

--- app/Http/Controllers/HRAdmin/StatisticsReportController.php
+++ app/Http/Controllers/HRAdmin/StatisticsReportController.php
@@ -844,7 +844,15 @@ class StatisticsReportController extends Controller
             $subset =$open_conversations->filter(function ($conversation) use($topic) {
                 return $conversation->conversation_topic_id == $topic->id;
             }); 
-            $subset = array_unique(array_column($subset->toArray(), 'employee_id'));
+
+            $subset = $subset->toArray();
+            foreach($subset as $index=>$value){
+                if($value['deleted_at'] != ''){
+                    unset($subset[$index]);
+                }
+            }
+            $subset = array_unique(array_column($subset, 'employee_id')); 
+            
             $unique_emp = count($subset);    
             $per_emp = 0;
             if($total_unique_emp > 0) {
@@ -869,7 +877,15 @@ class StatisticsReportController extends Controller
             $subset =$completed_conversations->filter(function ($conversation) use($topic) {
                 return $conversation->conversation_topic_id == $topic->id;
             }); 
-            $subset = array_unique(array_column($subset->toArray(), 'employee_id'));
+
+            $subset = $subset->toArray();
+            foreach($subset as $index=>$value){
+                if($value['deleted_at'] != ''){
+                    unset($subset[$index]);
+                }
+            }
+            $subset = array_unique(array_column($subset, 'employee_id')); 
+            
             $unique_emp = count($subset);    
             $per_emp = 0;
             if($total_unique_emp > 0) {
