d3da978534e9a74886ded432f4bc0b99e43710f4
jizhaogit <ji.zhao@telus.com>
2023-06-15 09:38:51 -0400

    Zenhub #898

--- app/Http/Controllers/SysAdmin/SysadminStatisticsReportController.php
+++ app/Http/Controllers/SysAdmin/SysadminStatisticsReportController.php
@@ -788,6 +788,49 @@ class SysadminStatisticsReportController extends Controller
         $data['chart4']['groups'] = array();
         foreach($topics as $topic)
         {
+            $employee_topic_query = UserDemoJrView::selectRaw("employee_id, empl_record, employee_name, 
+                                organization, level1_program, level2_division,
+                                level3_branch, level4,conversation_participants.role,
+                                conversations.deleted_at,conversation_participants.conversation_id,
+                                conversations.signoff_user_id,conversations.supervisor_signoff_id,
+                                conversation_participants.participant_id,conversations.conversation_topic_id,
+                        DATEDIFF ( next_conversation_date
+                            , curdate() )
+                    as overdue_in_days")
+                ->leftJoin('conversation_participants', function($join) {
+                    $join->on('conversation_participants.participant_id', '=', 'user_demo_jr_view.user_id');
+                })
+                ->leftJoin('conversations', function($join) {
+                    $join->on('conversations.id', '=', 'conversation_participants.conversation_id');
+                })        
+                ->where(function($query) {
+                    $query->where(function($query) {
+                        $query->where('due_date_paused', 'N')
+                            ->orWhereNull('due_date_paused');
+                    });
+                })
+                ->when($request->dd_level0, function ($q) use($request) { return $q->where('organization_key', $request->dd_level0); })
+                ->when($request->dd_level1, function ($q) use($request) { return $q->where('level1_key', $request->dd_level1); })
+                ->when($request->dd_level2, function ($q) use($request) { return $q->where('level2_key', $request->dd_level2); })
+                ->when($request->dd_level3, function ($q) use($request) { return $q->where('level3_key', $request->dd_level3); })
+                ->when($request->dd_level4, function ($q) use($request) { return $q->where('level4_key', $request->dd_level4); })
+                ->whereNull('date_deleted')
+                ->where('conversations.conversation_topic_id', $topic->id)
+                ->where(function($query) {
+                    $query->where(function($query) {
+                        $query->where('excused_flag', '<>', '1')
+                            ->orWhereNull('excused_flag');
+                    });
+                });  
+            $topic_employees = $employee_topic_query->get();              
+            
+            $conversations = $topic_employees->filter(function ($topic_employees) {
+                return $topic_employees->role == 'emp';
+            });
+            $open_conversations = $conversations->filter(function ($conversation) {
+                return $conversation->signoff_user_id === null || $conversation->supervisor_signoff_id === null;
+            }); 
+                        
             $subset =$open_conversations->filter(function ($conversation) use($topic) {
                 return $conversation->conversation_topic_id == $topic->id;
             }); 
@@ -819,7 +862,50 @@ class SysadminStatisticsReportController extends Controller
         $data['chart5']['groups'] = array();
         foreach($topics as $topic)
         {
-            $subset =$completed_conversations->filter(function ($conversation) use($topic) {
+            $employee_topic_query = UserDemoJrView::selectRaw("employee_id, empl_record, employee_name, 
+                                organization, level1_program, level2_division,
+                                level3_branch, level4,conversation_participants.role,
+                                conversations.deleted_at,conversation_participants.conversation_id,
+                                conversations.signoff_user_id,conversations.supervisor_signoff_id,
+                                conversation_participants.participant_id,conversations.conversation_topic_id,
+                        DATEDIFF ( next_conversation_date
+                            , curdate() )
+                    as overdue_in_days")
+                ->leftJoin('conversation_participants', function($join) {
+                    $join->on('conversation_participants.participant_id', '=', 'user_demo_jr_view.user_id');
+                })
+                ->leftJoin('conversations', function($join) {
+                    $join->on('conversations.id', '=', 'conversation_participants.conversation_id');
+                })        
+                ->where(function($query) {
+                    $query->where(function($query) {
+                        $query->where('due_date_paused', 'N')
+                            ->orWhereNull('due_date_paused');
+                    });
+                })
+                ->when($request->dd_level0, function ($q) use($request) { return $q->where('organization_key', $request->dd_level0); })
+                ->when($request->dd_level1, function ($q) use($request) { return $q->where('level1_key', $request->dd_level1); })
+                ->when($request->dd_level2, function ($q) use($request) { return $q->where('level2_key', $request->dd_level2); })
+                ->when($request->dd_level3, function ($q) use($request) { return $q->where('level3_key', $request->dd_level3); })
+                ->when($request->dd_level4, function ($q) use($request) { return $q->where('level4_key', $request->dd_level4); })
+                ->whereNull('date_deleted')
+                ->where('conversations.conversation_topic_id', $topic->id)
+                ->where(function($query) {
+                    $query->where(function($query) {
+                        $query->where('excused_flag', '<>', '1')
+                            ->orWhereNull('excused_flag');
+                    });
+                });  
+            $topic_employees = $employee_topic_query->get();              
+            
+            $conversations = $topic_employees->filter(function ($topic_employees) {
+                return $topic_employees->role == 'emp';
+            });
+            $complete_conversations = $conversations->filter(function ($conversation) {
+                return $conversation->signoff_user_id != null && $conversation->supervisor_signoff_id != null;
+            }); 
+                        
+            $subset =$complete_conversations->filter(function ($conversation) use($topic) {
                 return $conversation->conversation_topic_id == $topic->id;
             }); 
             $subset = $subset->toArray();
@@ -834,9 +920,9 @@ class SysadminStatisticsReportController extends Controller
             if($total_unique_emp > 0) {
                 $per_emp = ($unique_emp / $total_unique_emp) * 100;
             }
-            array_push( $data['chart5']['groups'],  [ 'name' => $topic->name, 'value' => $unique_emp, 
-                    'topic_id' => $topic->id, 
-                ]);
+            array_push( $data['chart5']['groups'],  [ 'name' => $topic->name, 'value' => $unique_emp,
+                        'topic_id' => $topic->id, 
+                        ]);
         } 
                 
         // Chart6 -- Employee Has Open Conversation
