3666e9d0a01b85f54b9af9f08755fa411b70a72b
jp-Telus <86217312+jp-Telus@users.noreply.github.com>
2023-06-27 13:53:54 -0700

    Merge branch 'dev' into bugfix-995

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', 'daily'),
 
     /*
     |--------------------------------------------------------------------------
@@ -49,9 +49,9 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/laravel-{date}.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 30,
         ],
 
         'slack' => [
