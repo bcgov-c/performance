05cb3237893c9adb52def7361001a4c5287e9805
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-06-28 09:26:14 -0700

    ZenHub #995 - Add Index To Speed Up Process
    ZenHub #995 - Add Index To Speed Up Process

---
+++ database/migrations/2023_06_28_011508_add_index_on_employee_demo_4.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexOnEmployeeDemo4 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->createTableIndexIfNotExist('employee_demo', 'idx_employee_demo_position_number_employee_id', ['position_number', 'employee_id']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->dropExistingTableIndex('employee_demo', 'idx_employee_demo_position_number_employee_id');
+    }
+
+    public function createTableIndexIfNotExist($tableName, $indexName, $fieldNames = []) {
+        $exists = DB::table('information_schema.statistics')
+            ->where('table_name', $tableName)
+            ->where('index_name', $indexName)
+            ->get()->count();
+        if (!$exists) {
+            Schema::table($tableName, function (Blueprint $table) use($indexName, $fieldNames) {
+                $table->index($fieldNames, $indexName);
+            });
+            echo " - Added index {$indexName} in {$tableName}"; echo "\r\n";
+        } else {
+            echo " - Index {$indexName} in {$tableName} already exist.  New index NOT added."; echo "\r\n";
+        }
+    }
+
+    public function dropExistingTableIndex($tableName, $indexName) {
+        $exists = DB::table('information_schema.statistics')
+            ->where('table_name', $tableName)
+            ->where('index_name', $indexName)
+            ->get()->count();
+        if ($exists) {
+            Schema::table($tableName, function($table) use($tableName, $indexName) {
+                $table->dropIndex($indexName);
+            });
+            echo " - Dropped index {$indexName} in {$tableName}"; echo "\r\n";
+        } else {
+            echo " - Index {$indexName} in {$tableName} NOT found.  Nothing to drop."; echo "\r\n";
+        }
+    }
+
+
+}
