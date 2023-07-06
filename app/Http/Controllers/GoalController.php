aace533d0692a4222c231b569fd4e51c924fba7c
JonTelus <jonathan.wood@telus.com>
2023-07-06 08:19:18 -0700

    Revert "Merge branch 'dev' of https://github.com/bcgov/performance into dev"
    This reverts commit 28bc829e4be2b26710a67257fadc07761df48f5d, reversing
    changes made to c5ba3f84636444c27f34fb3997cc0adb5d705f8a.

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -142,12 +142,7 @@ class GoalController extends Controller
             $goals = $user->sharedGoals()->paginate(8);
             return view('goal.index', compact('goals', 'type', 'goaltypes','goal_types_modal','user', 'tags', 'type_desc_str'));
         } else {
-            $query = $query->where('status', '<>', 'active')
-            ->select('goals.*', DB::raw('group_concat(distinct tags.name separator ", ") as tagnames')
-                    ,DB::raw('group_concat(distinct goals_shared_with.user_id separator ",") as shared_user_id')
-                    ,DB::raw('group_concat(distinct shared_users.name separator ",") as shared_user_name')
-                    ,'goal_types.name as typename');  
-            //$query = $query->where('status', '<>', 'active')->select('goals.*', DB::raw('group_concat(distinct tags.name separator ", ") as tagnames'), 'goal_types.name as typename');
+            $query = $query->where('status', '<>', 'active')->select('goals.*', DB::raw('group_concat(distinct tags.name separator ", ") as tagnames'), 'goal_types.name as typename');
         }
         
         
