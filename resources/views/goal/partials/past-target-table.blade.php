dc9520a879f4d5347113704e67085d9a327d5094
jizhaogit <ji.zhao@telus.com>
2023-07-11 13:10:22 -0400

    Zenhub #960 Add "Shared With" column to Past Goals list

--- resources/views/goal/partials/past-target-table.blade.php
+++ resources/views/goal/partials/past-target-table.blade.php
@@ -66,7 +66,7 @@
         @if($goal->login_role == 'owner') 
           @include('goal.partials.status-change')
         @else
-           {{$goal->status}}  
+          <x-goal-status :status="$goal->status"></x-goal-status>
         @endif
         
       </td>
