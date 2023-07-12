8e48680e048d8e642888f9d722672334c7480612
jizhaogit <ji.zhao@telus.com>
2023-07-12 13:46:52 -0400

    Zenhub #1031 Resources > Replace the FAQ content

---
+++ resources/views/resource/partials/faq/7.blade.php
@@ -0,0 +1,25 @@
+<p>
+Reporting relationship data is pulled from PeopleSoft (our HR system of record) once every 24 hours. This data cannot be modified in the PDP.
+</p>
+<p>
+If you want to add a shared supervisor for an employee in the PDP, you can have the current supervisor share the employee by clicking on the 
+Yes / No under the “Shared” column on their My Team page.
+</p>
+<p>
+If the data in PeopleSoft is incorrect, you will need to submit a service request through AskMyHR using the category 
+HR Software Systems Support > Position / Reporting Updates.
+</p>
+<p>
+<b>Note:</b> Employees that report to a position number in PeopleSoft that is vacant are automatically assigned one level higher in the organization hierarchy. 
+If that next level is also vacant, the employee will see on their home page that their current supervisor is “No supervisor”.
+</p>
+<p>
+<b>Note:</b> Employees that report to a position number in PeopleSoft that has more than one active employee associated with it 
+(“double-bunked” positions) will see a dropdown menu on their homepage that allows them to select which of the supervisors associated with the position number 
+is correct for them.
+</p>
+<p>
+If a supervisor shares a position number with one or more additional employees in PeopleSoft (i.e. the supervisor is “double-bunked” in their position) 
+they may not see the correct employees by default. In this case, their employees will be able to select the correct supervisor from a dropdown menu 
+on the homepage under the “Current supervisor” heading. Once the employee has made the selection, the supervisor will gain access to that employee in the PDP.
+</p>
