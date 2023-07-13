f5ffbcbf5adabf6e235f9ab968aa0c47cbdd5e1f
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-07-13 09:27:21 -0700

    ZenHub #906 - Additional Text
    ZenHub #906 - Additional Text

--- resources/views/resource/contact.blade.php
+++ resources/views/resource/contact.blade.php
@@ -1,28 +1,29 @@
 @extends('resource.layout')
 @section('tab-content')
 
+<div><p>If you need assistance and can't find the answer in the resources section, please send an email to your organization's PDP contact listed below.</p></div>
+
 <div id="accordion">
 	@foreach($data as $index=> $question)
-
-	<div class="card">
-		<div class="card-header" id="heading_{{$index}}">		
-		<h5 class="mb-0"data-toggle="collapse" data-target="#collapse_{{$index}}" aria-expanded="{{$index === 0 && ($t === '' || $t === 0) ? 'true' : 'false'}}" aria-controls="collapse_{{$index}}">
-			<button class="btn btn-link">
-			{{ $question['question'] }}
-			</button>
-		</h5>
+		<div class="card">
+			<div class="card-header" id="heading_{{$index}}">		
+			<h5 class="mb-0"data-toggle="collapse" data-target="#collapse_{{$index}}" aria-expanded="{{$index === 0 && ($t === '' || $t === 0) ? 'true' : 'false'}}" aria-controls="collapse_{{$index}}">
+				<button class="btn btn-link">
+				{{ $question['question'] }}
+				</button>
+			</h5>
+			</div>
+			
+			<div id="collapse_{{$index}}" class="collapse" aria-labelledby="heading_{{$index}}" data-parent="#accordion">
+			<div class="card-body">
+				@if (array_key_exists('answer', $question))
+					{{ $question['answer']}}
+				@else
+					@include('resource.partials.contact.'.$question['answer_file'])
+				@endif
+			</div>
+			</div>
 		</div>
-		
-		<div id="collapse_{{$index}}" class="collapse" aria-labelledby="heading_{{$index}}" data-parent="#accordion">
-		<div class="card-body">
-			@if (array_key_exists('answer', $question))
-				{{ $question['answer']}}
-			@else
-				@include('resource.partials.contact.'.$question['answer_file'])
-			@endif
-		</div>
-		</div>
-	</div>
 	@endforeach
 
 </div>
