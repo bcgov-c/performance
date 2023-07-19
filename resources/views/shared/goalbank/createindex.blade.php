af1dcf37384c75445cadf8448c70f92c3980a1ea
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-07-19 08:43:50 -0700

    ZenHub #1062 - Fix Goal Bank Individual modal message
    ZenHub #1062 - Fix Goal Bank Individual modal message

--- resources/views/shared/goalbank/createindex.blade.php
+++ resources/views/shared/goalbank/createindex.blade.php
@@ -47,7 +47,6 @@
 				<div class="col-md-4">
 					<b>Tags</b>
 					<i class="fa fa-info-circle" id="tags_label" data-trigger='click' data-toggle="popover" data-placement="right" data-html="true" data-content="Tags help to more accurately identity, sort, and report on your goals. You can add more than one tag to a goal. The list of tags will change and grow over time. <br/><br/>Don't see the goal tag you are looking for? <a href='mailto:performance.development@gov.bc.ca?subject=Suggestion for New Goal Tag'>Suggest a new goal tag</a>."></i>					
-					<!-- <x-dropdown :list="$tags" data-tooltip-trigger='hover' data-toggle="tooltip" name="tag_ids[]" id="tags" class="tags" multiple/>					 -->
 					<x-dropdown :list="$tags" name="tag_ids[]" id="tags" class="tags" multiple/>						
 					@if(session()->has('tags_miss'))
                                             <small class="text-danger">The tags field is required</small>
@@ -234,8 +233,6 @@
 				position: fixed;
 				top: 25%;
 				left: 47%;
-				/* height: 100%;
-				width: 100%; */
 				width: 10em;
 				height: 10em;
 				z-index: 9000000;
@@ -265,7 +262,6 @@
 				$(".tags").multiselect({
                 	enableFiltering: true,
                 	enableCaseInsensitiveFiltering: true,
-					// nonSelectedText: null,
             	});
 
 				$('body').on('click', function (e) {
@@ -326,7 +322,6 @@
                                 
                                 
 				$( "#btn_send" ).click(function() {
-					// $('#btn_send').prop('disabled',true);
 					$('#saveGoalModal').modal('toggle');
 				});
 
@@ -514,7 +509,6 @@
 
 				function eredrawTreeCheckboxes() {
 					// redraw the selection 
-					//console.log('eredraw triggered');
 					enodes = $('#eaccordion-level0 input:checkbox');
 					$.each( enodes, function( index, chkbox ) {
 						if (eg_employees_by_org.hasOwnProperty(chkbox.value)) {
@@ -700,7 +694,6 @@
 							})
 							
 						).then(function( data, textStatus, jqXHR ) {
-							//alert( jqXHR.status ); // Alerts 200
 							enodes = $('#eaccordion-level0 input:checkbox');
 							eredrawTreeCheckboxes();	
 						}); 
