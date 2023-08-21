31faa0211e2925d0b27f3c79a47a4f6e44ce71be
jizhaogit <ji.zhao@telus.com>
2023-08-21 15:47:53 -0400

    Zenhub #1094 Add private goals to the statistic goal summary

--- app/Http/Controllers/HRAdmin/StatisticsReportController.php
+++ app/Http/Controllers/HRAdmin/StatisticsReportController.php
@@ -928,7 +928,7 @@ class StatisticsReportController extends Controller
             $from_stmt_4 = $this->goalSummary_from_statement(4);
             $sql_4 = UserDemoJrView::selectRaw('A.*, goals_count, user_demo_jr_view.employee_name, 
                     user_demo_jr_view.organization, user_demo_jr_view.level1_program, user_demo_jr_view.level2_division, user_demo_jr_view.level3_branch, user_demo_jr_view.level4, user_demo_jr_view.reporting_to_name')
-                    ->from(DB::raw( $from_stmt_3 ))                                
+                    ->from(DB::raw( $from_stmt_4 ))                                
                     ->join('user_demo_jr_view', function($join) {
                         $join->on('user_demo_jr_view.employee_id', '=', 'A.employee_id');
                         //$join->on('employee_demo.empl_record', '=', 'A.empl_record');
