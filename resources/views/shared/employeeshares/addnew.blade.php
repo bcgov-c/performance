846a32e6843c44f1931e66faf2f280f61425acd6
jizhaogit <ji.zhao@telus.com>
2023-08-22 13:55:38 -0400

    Zenhub #1085 Replace Employee ID with Employee Email in Dropdown Menus

--- resources/views/shared/employeeshares/addnew.blade.php
+++ resources/views/shared/employeeshares/addnew.blade.php
@@ -829,6 +829,11 @@
             });
 
             $(".share-with-users").select2({
+                language: {
+                        errorLoading: function () {
+                            return "Searching for results.";
+                        }
+                        },
                 width: '100%',
                 ajax: {
                     url: '/users',
