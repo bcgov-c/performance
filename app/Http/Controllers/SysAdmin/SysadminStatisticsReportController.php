a8ed97e6f9e7dd8e226c2ce2d61c739c6855409f
jizhaogit <ji.zhao@telus.com>
2023-08-25 10:39:16 -0400

    Zenhub #1069

--- app/Http/Controllers/SysAdmin/SysadminStatisticsReportController.php
+++ app/Http/Controllers/SysAdmin/SysadminStatisticsReportController.php
@@ -492,10 +492,8 @@ class SysadminStatisticsReportController extends Controller
         if($request->dd_level4) {
             $count_raw .= " and user_demo_jr_view.level4_key = '".$request->dd_level4."'";
         }
-        $count_raw .= "     and ( ";
-        $count_raw .= "           user_demo_jr_view.due_date_paused = 'N' ";
-        $count_raw .= "         )";
-        $count_raw .= " and user_demo_jr_view.excused_flag <> 1 and user_demo_jr_view.date_deleted is null ";
+        $count_raw .= "     and (  user_demo_jr_view.due_date_paused = 'N' or user_demo_jr_view.excused_flag <> 1  )";
+        $count_raw .= "  and user_demo_jr_view.date_deleted is null ";
         $count_raw .= "     and goals.deleted_at is null   ";
         $count_raw .= "     and goals.is_library = 0   ";
         $count_raw .= "     and goals.status = 'active'   ";
@@ -525,7 +523,7 @@ class SysadminStatisticsReportController extends Controller
                         });
                     }) 
                 ->whereNull('user_demo_jr_view.date_deleted') 
-                //->where('goal_types.name','<>', 'Private')      
+                //->where('goal_types.name','<>', 'Private')  
                 ->whereNotExists(function ($query) {
                     $query->select(DB::raw(1))
                           ->from('goal_tags')
@@ -534,7 +532,7 @@ class SysadminStatisticsReportController extends Controller
                 ->where('user_demo_jr_view.guid', '<>', '')
                 ->where('goals.status', '=', 'active')
                 ->whereNull('goals.deleted_at')
-                ->where('goals.goal_type_id', '<>', 4)
+                //->where('goals.goal_type_id', '<>', 4)
                 ->where('goals.is_library', '=', 0);
                 
         $tags = $sql->get();
