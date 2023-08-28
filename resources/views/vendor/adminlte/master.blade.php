9d868373dadf9ebdf0b1a52fa71e42f2b2ee85e3
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-08-28 10:46:59 -0700

    ZenHub #1093 - Snowplow Changes
    ZenHub #1093 - Snowplow Changes

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -218,32 +218,28 @@
 
     <script>
         // <!-- Snowplow starts plowing - Standalone vE.2.14.0 -->
-        
-        ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
-        p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
-        };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
-        n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","https://www2.gov.bc.ca/StaticWebResources/static/sp/sp-2-14-0.js","snowplow"));
-            @switch (env('APP_ENV')) 
-            @case ('prod') 
-                var collector = 'spt.apps.gov.bc.ca';
-                @break;
-            @default
-                var collector = 'spm.apps.gov.bc.ca';
-        @endswitch
-        window.snowplow('newTracker','rt',collector, {
-        appId: 'Snowplow_standalone_PSA',
-        cookieLifetime: 86400 * 548,
-        platform: 'web',
-        post: true,
-        forceSecureTracker: true,
-        contexts: {
-        webPage: true,
-        performanceTiming: true
+        var collector = '{{ env('SNOWPLOW_COLLECTOR') }}';
+        if(collector != null && collector != ''){
+            console.log(collector);
+            ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
+            p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
+            };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
+            n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","https://www2.gov.bc.ca/StaticWebResources/static/sp/sp-2-14-0.js","snowplow"));
+            window.snowplow('newTracker','rt',collector, {
+            appId: 'Snowplow_standalone_PSA',
+            cookieLifetime: 86400 * 548,
+            platform: 'web',
+            post: true,
+            forceSecureTracker: true,
+            contexts: {
+            webPage: true,
+            performanceTiming: true
+            }
+            });
+            window.snowplow('enableActivityTracking', 30, 30); // Ping every 30 seconds after 30 seconds
+            window.snowplow('enableLinkClickTracking');
+            window.snowplow('trackPageView');
         }
-        });
-        window.snowplow('enableActivityTracking', 30, 30); // Ping every 30 seconds after 30 seconds
-        window.snowplow('enableLinkClickTracking');
-        window.snowplow('trackPageView');
         // <!-- Snowplow stops plowing –>
     </script>
 
