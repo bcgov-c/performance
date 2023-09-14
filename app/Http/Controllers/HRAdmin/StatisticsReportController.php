f0d07c16b97c58886602a5e9752e08ca6c2b5e92
jizhaogit <ji.zhao@telus.com>
2023-09-14 12:11:57 -0400

    Zenhub #1027 Stats and Reporting > Excused Employees > Update excel download file

--- app/Http/Controllers/HRAdmin/StatisticsReportController.php
+++ app/Http/Controllers/HRAdmin/StatisticsReportController.php
@@ -2479,7 +2479,7 @@ class StatisticsReportController extends Controller
         );
 
         $columns = ["Employee ID", "Name", "Email", 
-                        "Excused", "Reason", "Excused By", "Excused At", "Updated At",
+                        "Excused", "Reason", "Excused By", "Excused At", 
                         "Organization", "Level 1", "Level 2", "Level 3", "Level 4",
                     ];
 
@@ -2501,8 +2501,8 @@ class StatisticsReportController extends Controller
                 } else {
                     $row['Excused By'] = '';
                 }
-                $row['Excused At'] = $user->excused_start_date;
-                $row['Updated At'] = $user->excused_updated_at;
+                //$row['Excused At'] = $user->excused_start_date;
+                $row['Excused At'] = $user->excused_updated_at;
 
                 $row['Organization'] = $user->organization;
                 $row['Level 1'] = $user->level1_program;
@@ -2511,7 +2511,7 @@ class StatisticsReportController extends Controller
                 $row['Level 4'] = $user->level4;
 
                 fputcsv($file, array($row['Employee ID'], $row['Name'], $row['Email'], 
-                        $row['Excused'], $row['Reason'], $row['Excused By'], $row['Excused At'],$row['Updated At'],
+                        $row['Excused'], $row['Reason'], $row['Excused By'], $row['Excused At'],
                         $row['Organization'], $row['Level 1'], $row['Level 2'], $row['Level 3'], $row['Level 4'] ));
             }
 
