4a9d871c5cc12786c39ba68ba69bcb8b7f078d4f
jizhaogit <ji.zhao@telus.com>
2023-09-25 18:53:15 -0400

    Zenhub #583 Unauthorized Page Access by typing specific URL on the browser
    Zenhub #583 Unauthorized Page Access by typing specific URL on the browser

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -22,3 +22,32 @@
 @section('js')
     {{ $js ?? '' }}
 @stop
+
+
+
+<script>
+@if(session()->has('sr_user'))
+    var routeUrl = "{{ route('dashboard') }}";
+    var pathname = window.location.pathname;
+    var segments = pathname.split('/');
+    var controllerName = segments[1];
+    if(controllerName == 'hradmin' || controllerName == 'sysadmin') {
+        window.location.href = routeUrl;
+    }
+@endif
+
+
+@if(session()->has('view-profile-as'))
+    @if(session()->has('GOALS_ALLOWED'))
+        var routeUrl = "{{ route('goal.current') }}";
+    @else
+        var routeUrl = "{{ route('conversation.upcoming') }}";
+    @endif
+    var pathname = window.location.pathname;
+    var segments = pathname.split('/');
+    var controllerName = segments[1];
+    if(controllerName == 'dashboard' || controllerName == 'my-team' || controllerName == 'resources') {
+        window.location.href = routeUrl;
+    }
+@endif
+</script>    
