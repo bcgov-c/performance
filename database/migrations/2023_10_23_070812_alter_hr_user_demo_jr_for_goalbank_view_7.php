90b0ffea0682a081a93a0970549846d97f4a3b94
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-10-24 10:35:48 -0700

    ZenHub #857 - View Updates
    ZenHub #857 - View Updates

---
+++ database/migrations/2023_10_23_070812_alter_hr_user_demo_jr_for_goalbank_view_7.php
@@ -0,0 +1,128 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterHrUserDemoJrForGoalbankView7 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::statement("
+            ALTER VIEW hr_user_demo_jr_for_goalbank_view
+            AS
+            SELECT 
+                au.auth_id,
+                u.id AS user_id,
+                u.name AS user_name,
+                u.employee_id,
+                u.guid,
+                u.excused_flag,
+                u.excused_reason_id,
+                u.excused_updated_by,
+                u.excused_updated_at,
+                u.joining_date,
+                u.acctlock,
+                u.reporting_to,
+                d.empl_record,
+                d.employee_name,
+                d.employee_email,
+                d.jobcode,
+                d.jobcode_desc,
+                d.job_indicator,
+                d.orgid,
+                ua.level,
+                ua.organization,
+                ua.level1_program,
+                ua.level2_division,
+                ua.level3_branch,
+                ua.level4,
+                ua.level5,
+                ua.level6,
+                ua.organization_key,
+                ua.level1_key,
+                ua.level2_key,
+                ua.level3_key,
+                ua.level4_key,
+                ua.level5_key,
+                ua.level6_key,
+                ua.organization_deptid,
+                ua.level1_deptid,
+                ua.level2_deptid,
+                ua.level3_deptid,
+                ua.level4_deptid,
+                ua.level5_deptid,
+                ua.level6_deptid,
+                d.deptid,
+                d.employee_status,
+                d.position_number,
+                d.manager_id,
+                d.supervisor_position_number,
+                d.supervisor_emplid,
+                d.supervisor_name,
+                d.supervisor_email,
+                ua.reporting_to_employee_id,
+                ua.reporting_to_name,
+                ua.reporting_to_email,
+                ua.reporting_to_position_number,
+                d.date_updated,
+                d.date_deleted,
+                ua.jr_id,
+                ua.jr_due_date_paused AS due_date_paused,
+                ua.jr_next_conversation_date AS next_conversation_date,
+                ua.jr_excused_type AS excused_type,
+                ua.jr_current_manual_excuse AS current_manual_excuse,
+                ua.jr_created_by_id AS created_by_id,
+                ua.jr_created_at AS created_at,
+                ua.jr_updated_by_id AS updated_by_id,
+                ua.jr_updated_at AS updated_at,
+                ua.jr_excused_reason_id AS edj_excused_reason_id,
+                ua.jr_excused_reason_desc AS edj_excused_reason_desc,
+                ua.jr_updated_by_name AS updated_by_name,
+                ua.excused_updated_by_name,
+                ua.r_name,
+                ua.reason_id,
+                ua.reason_name,
+                ua.excusedtype,
+                ua.excusedlink,
+                ua.excused_by_name,
+                ua.created_at_string,
+                u.employee_id AS employee_id_search,
+                d.employee_name AS employee_name_search,
+                ua.isSupervisor,
+                ua.isDelegate
+            FROM 
+                auth_users AS au
+                INNER JOIN users AS u 
+                    USE INDEX (idx_users_id)
+                    ON u.id = au.user_id
+                INNER JOIN employee_demo AS d 
+                    USE INDEX (idx_employee_demo_employeeid_orgid)
+                    ON d.employee_id = u.employee_id
+                INNER JOIN users_annex AS ua 
+                    USE INDEX (users_annex_employee_id_record_index)
+                    ON ua.employee_id = d.employee_id
+                        AND ua.empl_record = d.empl_record
+                INNER JOIN auth_orgs AS ao
+                    USE INDEX (idx_auth_orgs_type_auth_id_orgid)
+                    ON ao.type = 'HR' AND ao.auth_id = au.auth_id 
+                        AND ao.orgid = d.orgid
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+
+}
