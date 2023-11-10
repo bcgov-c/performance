43249ed2e70d74f35bd50c91c327a2a29bb4df0a
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-11-09 18:29:34 -0800

    ZenHub #857 - Users Annex Changes
    ZenHub #857 - Users Annex Changes

--- app/Models/UsersAnnex.php
+++ app/Models/UsersAnnex.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class UsersAnnex extends Model
 {
 
-    public $table = 'users_annex';
+    public $table = 'users_annex'; 
     public $timestamps = true;
     public $incrementing = true;
 
