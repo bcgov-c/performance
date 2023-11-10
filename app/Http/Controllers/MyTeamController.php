03b8109f16e4f15a28ae8f9e5c921d5049b1e5c9
jizhaogit <ji.zhao@telus.com>
2023-11-10 18:15:45 -0500

    Zenhub #1157 Update goal sharing dropdown list UI: first employee in list receiving dozens of incorrect shares.

--- app/Http/Controllers/MyTeamController.php
+++ app/Http/Controllers/MyTeamController.php
@@ -360,19 +360,19 @@ class MyTeamController extends Controller
         $search = $request->search;
         
         if ($current_user == '') {
-            $user_query = User::select('id', 'name', 'email')
+            $user_query = User::select('id', 'name', 'employee_demo.employee_email')
                           ->where('name', 'LIKE', "%{$search}%")
                           ->join('employee_demo', 'employee_demo.employee_id','users.employee_id')
                           ->whereNull('employee_demo.date_deleted')  
-                          ->groupBy('id', 'name', 'email')
+                          ->groupBy('id', 'name', 'employee_demo.employee_email')
                           ->paginate();
         } else {
-            $user_query = User::select('id', 'name', 'email')
+            $user_query = User::select('id', 'name', 'employee_demo.employee_email')
                           ->where('name', 'LIKE', "%{$search}%")
                           ->where('id', '<>', $current_user)
                           ->join('employee_demo', 'employee_demo.employee_id','users.employee_id')
                           ->whereNull('employee_demo.date_deleted')  
-                          ->groupBy('id', 'name', 'email')
+                          ->groupBy('id', 'name', 'employee_demo.employee_email')
                           ->paginate();
         }
         
