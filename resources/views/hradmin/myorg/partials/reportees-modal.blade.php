f69e451b969b2ebdb7662d8ffab41d6568ad9e79
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2023-11-09 20:22:15 -0800

    ZenHub #779 - Code Sync From Test To Training
    ZenHub #779 - Code Sync From Test To Training

--- resources/views/hradmin/myorg/partials/reportees-modal.blade.php
+++ resources/views/hradmin/myorg/partials/reportees-modal.blade.php
@@ -5,13 +5,13 @@
     <div class="modal-dialog modal-lg modal-dialog-scrollable modal-dialog-centered"> 
         <div class="modal-content"> 
             <div class="modal-header bg-primary"> 
-                <h5 class="modal-title" id="reporteesTitle">{{__('Reports List')}}</h5> 
+                <h5 class="modal-title" id="reporteesTitle">{{__('Direct / Shared Reports')}}</h5> 
                 <button type="button" class="close" id="close-reportees-modal" name="close-reportees-modal" data-dismiss="modal" aria-label="Close"> 
                     <span aria-hidden="true">&times;</span> 
                 </button> 
             </div> 
             <div class="p-3"> 
-                <strong>Below are the reportees</strong> <br> 
+                <!-- <strong>Below are the reportees</strong> <br>  -->
                 <table class="table table-bordered reporteesTable table-striped" id="reporteesTable" name="reporteesTable" style="width: 100%; overflow-x: auto; "></table>
             </div> 
         </div> 
