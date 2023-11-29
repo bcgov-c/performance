53fe8298ae2944297439934560ebab30e844f37b
jizhaogit <ji.zhao@telus.com>
2023-11-29 14:52:05 -0500

    Zenhub #1172 Missing full list of goal tags in resources section

--- resources/views/components/xdropdown.blade.php
+++ resources/views/components/xdropdown.blade.php
@@ -2,7 +2,7 @@
 <label>
     {{ __($label ?? '') }}
     <!-- {!! $label ?? '' ? ' <i id="tag-info" class="fas fa-info-circle" data-trigger=" click " data-toggle-select="popover" data-placement="right" data-html="true" data-content="Tags help to more accurately identity, sort, and report on your goals. You can add more than one tag to a goal. The list of tags will change and grow over time. <br/><br/><a href=\'/resource/goal-setting?t=5\' target=\'_blank\'><u>View full list of tag descriptions.</u></a>" data-original-title=""></i><br>': ''!!} -->
-    {!! $label ?? '' ? ' <i id="tag-info" class="fas fa-info-circle" data-trigger=" click " data-toggle-select="popover" data-placement="right" data-html="true" data-content="Tags help to more accurately identity, sort, and report on your goals. You can add more than one tag to a goal. The list of tags will change and grow over time. <br/><br/><a href=\'/resources/goal-setting?t=5\' target=\'_blank\'><u>View full list of tag descriptions.</u></a>" data-original-title=""></i><br>': ''!!}
+    {!! $label ?? '' ? ' <i id="tag-info" class="fas fa-info-circle" data-trigger=" click " data-toggle-select="popover" data-placement="right" data-html="true" data-content="Tags help to more accurately identity, sort, and report on your goals. You can add more than one tag to a goal. The list of tags will change and grow over time. <br/><br/><a href=\'/resources/goal-setting?t=8\' target=\'_blank\'><u>View full list of tag descriptions.</u></a>" data-original-title=""></i><br>': ''!!}
     
     <select {!! $attributes->merge(['class' => 'form-control']) !!}>
         @if($blankOptionText != null)
