f3c590a68d6bdbfebfbc9835d7b3d7131142640b
jizhaogit <ji.zhao@telus.com>
2023-11-30 12:57:11 -0500

    Zenhub #1159 Resources: add tab to link users to their old MyPerformance profile from within the PDP

---
+++ resources/views/resource/access-performance.blade.php
@@ -0,0 +1,61 @@
+@extends('resource.layout')
+@section('tab-content')
+
+<style>
+.card a {
+  text-decoration: underline;
+}
+</style>
+
+<div id="accordion">
+	@foreach($data as $index=> $question)
+
+	<div class="card">
+		<div class="card-header" id="heading_{{$index}}">		
+		<h5 class="mb-0"data-toggle="collapse" data-target="#collapse_{{$index}}" aria-expanded="{{$index === 0 && ($t === '' || $t === 0) ? 'true' : 'false'}}" aria-controls="collapse_{{$index}}">
+			<button class="btn btn-link">
+			{{ $question['question'] }}
+			</button>
+		</h5>
+		</div>
+		
+		<div id="collapse_{{$index}}" class="collapse {{$index == $t ? 'show' : ''}}" aria-labelledby="heading_{{$index}}" data-parent="#accordion">
+		<div class="card-body">
+			@if (array_key_exists('answer', $question))
+				{!! $question['answer'] !!}
+			@else
+				@include('resource.partials.user-guide.'.$question['answer_file'])
+			@endif
+		</div>
+		</div>
+	</div>
+	@endforeach
+
+</div>
+
+@push('css')
+<style>
+	.card-header{
+		cursor: pointer;
+	}
+</style>
+@endpush
+	@push('js')
+    	<script>
+            $(document).ready(function(){
+                $('[data-toggle="popover"]').popover();
+            });
+
+            $('body').on('click', function (e) {
+                $('[data-toggle=popover]').each(function () {
+                    // hide any open popovers when the anywhere else in the body is clicked
+                    if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                        $(this).popover('hide');
+                    }
+                });
+            });
+      	</script>
+	@endpush
+@endsection
+
+
