48c8dece50e823c4f6187a955c162b140bea3c5f
jizhaogit <ji.zhao@telus.com>
2024-03-07 10:06:52 -0500

    Zenhub #1140 Checkbox enhancement

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -2,12 +2,12 @@
 <label>
     {{ __($label ?? '') }}
     {!! $label ?? '' ? '<br>': ''!!}
-    <select {!! $attributes->merge(['class' => 'form-control']) !!}>
+    <select {!! $attributes->merge(['class' => 'form-control']) !!}  aria-haspopup="true" aria-expanded="false">>
         @if($blankOptionText != null)
-            <option value="">{{$blankOptionText}}</option>
+            <option value="" aria-labelledby="{{$blankOptionText}}">{{$blankOptionText}}</option>
         @endif
         @foreach ($list as $item)
-            <option value="{{ $item['id'] }}" {{ ($item['selected'] ?? '') || $selected != null && ($item['id'] == $selected || (is_array($selected) && (in_array($item['id'], $selected)))) ? 'selected': '' }}>
+            <option aria-labelledby="{{ $item['name'] }}" value="{{ $item['id'] }}" {{ ($item['selected'] ?? '') || $selected != null && ($item['id'] == $selected || (is_array($selected) && (in_array($item['id'], $selected)))) ? 'selected': '' }}>
                 {{ $item['name'] }}
             </option>
         @endforeach
