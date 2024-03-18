eb076c9fd0d31142ed5c6f1ea70ac6804921a3fe
jizhaogit <ji.zhao@telus.com>
2024-03-18 10:52:01 -0400

    Zenhub #1140 Checkbox enhancement

--- resources/views/dashboard/partials/tabs.blade.php
+++ resources/views/dashboard/partials/tabs.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex justify-content-center justify-content-lg-start mb-2">
-    <div role="region" aria-label="notifications" class="px-4 py-1 border-bottom {{$tab == 'notifications' ? 'border-primary' : ''}}">
+    <div aria-label="notifications" class="px-4 py-1 border-bottom {{$tab == 'notifications' ? 'border-primary' : ''}}">
         <x-button style="-" :href="route('dashboard')">
             Notifications
             <span id="count-badge" class="badge badge-{{$tab == 'notifications' ? 'primary' : 'secondary' }}"></span>
