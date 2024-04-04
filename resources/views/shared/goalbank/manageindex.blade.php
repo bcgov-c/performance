7a2f9c1b7598d973cab4797c1060752f41bda1ed
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2024-04-04 01:52:32 -0700

    ZenHub #1257 - Additional fixes for #1251
    ZenHub #1257 - Additional fixes for #1251

--- resources/views/shared/goalbank/manageindex.blade.php
+++ resources/views/shared/goalbank/manageindex.blade.php
@@ -130,19 +130,19 @@
                     },
                     columns: 
                     [
-                        {title: 'Goal Title', ariaTitle: 'Goal Title', target: 0, orderData: [0, 10], type: 'string', data: 'click_title', name: 'click_title', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Goal Type', ariaTitle: 'Goal Type', target: 1, orderData: [1, 10], type: 'string', data: 'click_goal_type', name: 'click_goal_type', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Mandatory', ariaTitle: 'Mandatory', target: 2, orderData: [2, 10], type: 'string', data: 'mandatory', name: 'mandatory', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Goal Creation Date', ariaTitle: 'Goal Creation Date', target: 3, orderData: [3, 10], type: 'date', data: 'created_at', name: 'created_at', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Display Name', ariaTitle: 'Display Name', target: 4, orderData: [4, 10], type: 'string', data: 'click_display_name', name: 'click_display_name', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Created By', ariaTitle: 'Created By', target: 5, orderData: [5, 10], type: 'string', data: 'click_creator_name', name: 'click_creator_name', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Created By Organization', ariaTitle: 'Created By Organization', target: 6, orderData: [6, 10], type: 'string', data: 'click_creator_organization', name: 'click_creator_organization', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Last Modified Date', ariaTitle: 'Last Modified Date', target: 3, orderData: [3, 10], type: 'date', data: 'updated_at', name: 'updated_at', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Last Modified By', ariaTitle: 'Last Modified By', target: 5, orderData: [5, 10], type: 'string', data: 'click_updater_name', name: 'click_updater_name', searchable: true, className: 'dt-nowrap'},
-                        {title: 'Individual Audience', ariaTitle: 'Individual Audience', target: 8, orderData: [7, 10], type: 'num', data: 'audience', name: 'audience', searchable: true},
-                        {title: 'Business Unit Audience', ariaTitle: 'Business Unit Audience', target: 9, orderData: [8, 10], type: 'num', data: 'org_audience', name: 'org_audience', searchable: true},
-                        {title: 'Action', ariaTitle: 'Action', target: 9, orderData: [10, 11], type: 'string', data: 'action', name: 'action', orderable: false, searchable: false},
-                        {title: 'Goal ID', ariaTitle: 'Goal ID', target: 10, orderData: [11], type: 'string', data: 'id', name: 'id', searchable: false, visible: false},
+                        {title: 'Goal Title', ariaTitle: 'Goal Title', target: 0, orderData: [0, 12], type: 'string', data: 'click_title', name: 'click_title', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Goal Type', ariaTitle: 'Goal Type', target: 1, orderData: [1, 12], type: 'string', data: 'click_goal_type', name: 'click_goal_type', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Mandatory', ariaTitle: 'Mandatory', target: 2, orderData: [2, 12], type: 'string', data: 'mandatory', name: 'mandatory', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Goal Creation Date', ariaTitle: 'Goal Creation Date', target: 3, orderData: [3, 12], type: 'date', data: 'created_at', name: 'created_at', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Display Name', ariaTitle: 'Display Name', target: 4, orderData: [4, 12], type: 'string', data: 'click_display_name', name: 'click_display_name', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Created By', ariaTitle: 'Created By', target: 5, orderData: [5, 12], type: 'string', data: 'click_creator_name', name: 'click_creator_name', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Created By Organization', ariaTitle: 'Created By Organization', target: 6, orderData: [6, 12], type: 'string', data: 'click_creator_organization', name: 'click_creator_organization', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Last Modified Date', ariaTitle: 'Last Modified Date', target: 7, orderData: [7, 12], type: 'date', data: 'updated_at', name: 'updated_at', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Last Modified By', ariaTitle: 'Last Modified By', target: 8, orderData: [8, 12], type: 'string', data: 'click_updater_name', name: 'click_updater_name', searchable: true, className: 'dt-nowrap'},
+                        {title: 'Individual Audience', ariaTitle: 'Individual Audience', target: 9, orderData: [9, 12], type: 'num', data: 'audience', name: 'audience', searchable: true},
+                        {title: 'Business Unit Audience', ariaTitle: 'Business Unit Audience', target: 10, orderData: [10, 12], type: 'num', data: 'org_audience', name: 'org_audience', searchable: true},
+                        {title: 'Action', ariaTitle: 'Action', target: 11, orderData: [11, 12], type: 'string', data: 'action', name: 'action', orderable: false, searchable: false},
+                        {title: 'Goal ID', ariaTitle: 'Goal ID', target: 12, orderData: [12], type: 'string', data: 'id', name: 'id', searchable: false, visible: false},
                     ]
                 } );
             });
