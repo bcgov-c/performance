9f7329d153b716160f3d2dba0307ef6ad04ccc23
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2024-04-22 14:50:43 -0700

    ZenHub #1271 - Fix Cancel button in Sys/HR Admin Goals
    ZenHub #1271 - Fix Cancel button in Sys/HR Admin Goals

--- resources/views/shared/goalbank/editone.blade.php
+++ resources/views/shared/goalbank/editone.blade.php
@@ -110,7 +110,7 @@
 		<br>
 		<div class="col-md-3 mb-2">
 			<button class="btn btn-primary mt-2" type="button" onclick="confirmSaveChangesModal()" id="obtn_send" name="obtn_send" value="btn_send">Save Changes</button>
-			<button class="btn btn-secondary mt-2" id="obtn_cancel_send" name="obtn_cancel_send">Cancel</button>
+			<button class="btn btn-secondary mt-2" id="obtn_cancel_send" name="obtn_cancel_send" type="button" onClick="window.location='{{ URL::previous() }}'">Cancel</button>
 		</div>
 
 	</form>
