0c81e0e8a0ea87445e09424be6dc9e220d7016ce
jizhaogit <ji.zhao@telus.com>
2024-04-30 12:43:43 -0400

    Zenhub #1119 My Current Goals Page
    Zenhub #1119 My Current Goals Page
    
    Zenhub #1119 My Current Goals Page
    
    Zenhub #1119 My Current Goals Page
    
    Zenhub #1119 My Current Goals Page

--- resources/views/goal/partials/status-change.blade.php
+++ resources/views/goal/partials/status-change.blade.php
@@ -1,11 +1,11 @@
-<div class="btn-group">
-  <button type="button" class="btn btn-outline-secondary dropdown-toggle text-capitalize" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<div class="btn-group" tabindex="0">
+  <button type="button" aria-label="Current goal is marked as {{ $goal->status }}" class="btn btn-outline-secondary dropdown-toggle text-capitalize" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     <x-goal-status :status="$goal->status"></x-goal-status>
   </button>
   <div class="dropdown-menu goal-change">
     @foreach (\Config::get("global.status") as $status => $value)
         @if ($status != $goal->status)
-            <a class="dropdown-item text-capitalize" data-current-status="{{$goal->status}}" data-status="{{$status}}" href="{{ route('goal.update-status', [$goal->id, $status]) }}">
+            <a aria-label="Click on the button to mark the goal as {{ $status }}" class="dropdown-item text-capitalize" data-current-status="{{$goal->status}}" data-status="{{$status}}" href="{{ route('goal.update-status', [$goal->id, $status]) }}">
                 <x-goal-status :status="$status"></x-goal-status>
             </a>
         @endif
--- resources/views/goal/partials/target-table.blade.php
+++ resources/views/goal/partials/target-table.blade.php
@@ -2,24 +2,28 @@
 
    <table class="table ">
 
-  <thead>
+   <thead>
     <tr>
-      <th scope="col"><a href="javascript:sort('title');">Title</a></th>
-      <th scope="col"><a href="javascript:sort('goal_type_id');">Goal Type</a></th>
-      <th scope="col"><a href="javascript:sort('tagnames');">Tags</a></th>
-      <th scope="col"><a href="javascript:sort('start_date');">Start Date</a></th>
-      <th scope="col"><a href="javascript:sort('target_date');">End Date</a></th>
+      <th scope="col" style="width: 120px;"><a href="javascript:sort('title');">Title</a></th>
+      <th scope="col" style="width: 120px;"><a href="javascript:sort('goal_type_id');">Goal Type</a></th>
+      <th scope="col" style="width: 100px;"><a href="javascript:sort('tagnames');">Tags</a></th>
+      <th scope="col" style="width: 80px;"><a href="javascript:sort('start_date');">Start Date</a></th>
+      <th scope="col" style="width: 80px;"><a href="javascript:sort('target_date');">End Date</a></th>
       @if ($type == 'current')
-      <th scope="col">Shared With <i class="fa fa-info-circle" data-trigger="click" data-toggle="popover" data-placement="right" data-html="true" 
-      data-content="You can use this function to share a goal with colleagues. This goal will appear on their “Goals Shared with Me” tab and they will be able view and add comments to the goal. 
-      <br/>Type the name of your colleague into the search field to find and select them. You can repeat this process to add additional colleagues if needed. You can also remove a colleague by clicking the &quot;X&quot; next to their name.
-      <br/>As the creator of the shared goal, you are the only one that can make major edits or change the status to achieved or archived." 
-      data-original-title="" title="" aria-describedby="popover271882"> </i></th>
+      <th scope="col" style="width: 350px;">Shared With 
+        <i tabindex="0" class="fa fa-info-circle" data-trigger="focus" data-toggle="popover" data-placement="right" data-html="true" 
+          data-content="You can use this function to share a goal with colleagues. This goal will appear on their “Goals Shared with Me” tab and they will be able view and add comments to the goal. 
+          <br/>Type the name of your colleague into the search field to find and select them. You can repeat this process to add additional colleagues if needed. You can also remove a colleague by clicking the &quot;X&quot; next to their name.
+          <br/>As the creator of the shared goal, you are the only one that can make major edits or change the status to achieved or archived." 
+          data-original-title="" title="" aria-label="You can use this function to share a goal with colleagues. This goal will appear on their “Goals Shared with Me” tab and they will be able view and add comments to the goal. 
+          <br/>Type the name of your colleague into the search field to find and select them. You can repeat this process to add additional colleagues if needed. You can also remove a colleague by clicking the &quot;X&quot; next to their name.
+          <br/>As the creator of the shared goal, you are the only one that can make major edits or change the status to achieved or archived."> </i>
+    </th>
       @endif
-      <th scope="col"><a href="javascript:sort('status');">Status</a>
-      <i class="fa fa-info-circle" id="status_label" data-trigger='click' data-toggle="popover" data-placement="right" data-html="true" data-content="<ul><li><b>Active</b>: currently in progress or scheduled for a future date</li><li><b>Achieved</b>: supervisor and employee agree objectives met</li><li><b>Archived</b>: cancelled, deferred or no longer relevant to your work but you want to save for future reference</li></ul>You can delete goals that do not meet any of the above criteria"></i>				
+      <th scope="col" style="width: 60px;"><a href="javascript:sort('status');">Status</a>
+      <i tabindex="0" class="fa fa-info-circle" id="status_label" data-trigger='focus' data-toggle="popover" data-placement="right" data-html="true" data-content="<ul><li><b>Active</b>: currently in progress or scheduled for a future date</li><li><b>Achieved</b>: supervisor and employee agree objectives met</li><li><b>Archived</b>: cancelled, deferred or no longer relevant to your work but you want to save for future reference</li></ul>You can delete goals that do not meet any of the above criteria"></i>				
       </th>
-      <th> </th>
+      <th style="width: 150px;"> </th>
     </tr>
   </thead>
   <tbody>
@@ -53,7 +57,7 @@
       <td >{{ $goal->target_date_human }}</td>
       @if ($type == 'current')
       <td>  
-        <select multiple class="form-control share-with-users"  name="share_with_users[]">
+        <select multiple class="form-control share-with-users"  name="share_with_users[]" aria-label='Search and add employees from the list with whom you want to share the goal'>
             <?php if($goal->shared_user_id != '' && $goal->shared_user_name != ''){
                 $share_user_id_arr = explode(',', $goal->shared_user_id);
                 $share_user_name_arr = explode(',', $goal->shared_user_name);
@@ -69,12 +73,15 @@
         @include('goal.partials.status-change')
       </td>
       <td>
-        <div class="d-flex">          
+        <div class="d-flex" tabindex="-1">          
           <form id="delete-goal-{{$goal->id}}" action="{{ route('goal.destroy', $goal->id)}}" method="POST" onsubmit="return confirm('Are you sure you want to permanently delete this goal?')">
             @csrf
             @method('DELETE')
-            <x-button :href="route('goal.show', $goal->id)" size='sm' class="mr-2">View</x-button>
-            <x-button size='sm' icon='trash' style="danger"></x-button>
+            <button type="button" aria-label=" Click on the view button to view the goal" alt=" View the goal" 
+            onclick="viewGoal({{ $goal->id }})"  class="btn btn-outline-primary btn-sm ml-2 mt-2"  tabindex="0">View <i class="fas fa-eye fa-lg"></i></button>
+
+            <button type="submit" aria-label="Click on the delete button to delete the goal" alt="Delete the goal" 
+            class="btn btn-outline-danger btn-sm ml-2 delete-dn mt-2"   tabindex="0">Delete <i class="fas fa-trash-alt fa-lg"></i></button>
           </form>
         </div>
       </td>
@@ -84,3 +91,10 @@
 
 </table>
 </div>
+
+<script>
+  function viewGoal(goalId) {
+      window.location.href = '/goal/ '+ goalId;
+  }
+
+</script>
