81d756dbd7e25914b6f188229edb34587e8e5a71
jizhaogit <ji.zhao@telus.com>
2024-05-07 13:06:37 -0400

    Zenhub #1119 My Current Goals Page

--- resources/views/goal/partials/status-change.blade.php
+++ resources/views/goal/partials/status-change.blade.php
@@ -1,3 +1,5 @@
+
+
 <div class="btn-group" tabindex="0">
   <button type="button" aria-label="Current goal is marked as {{ $goal->status }}" class="btn btn-outline-secondary dropdown-toggle text-capitalize" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     <x-goal-status :status="$goal->status"></x-goal-status>
@@ -12,3 +14,18 @@
     @endforeach
   </div>
 </div>
+
+
+<!-----
+<div class="goal-status-checkboxes" role="group" aria-label="Goal Status">
+    <label>
+        <input type="checkbox" class="goal-checkbox" aria-label="Mark the goal as Active" data-current-status="{{ $goal->status }}" data-status="Active" {{ $goal->status == 'Active' ? 'checked' : '' }}> Active
+    </label>
+    <label>
+        <input type="checkbox" class="goal-checkbox" aria-label="Mark the goal as Achieved" data-current-status="{{ $goal->status }}" data-status="Achieved" {{ $goal->status == 'Achieved' ? 'checked' : '' }}> Achieved
+    </label>
+    <label>
+        <input type="checkbox" class="goal-checkbox" aria-label="Mark the goal as Archived" data-current-status="{{ $goal->status }}" data-status="Archived" {{ $goal->status == 'Archived' ? 'checked' : '' }}> Archived
+    </label>
+</div>
+----->
