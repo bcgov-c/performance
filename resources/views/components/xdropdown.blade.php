93f5b3f97cb37f0eadeb9119302aa7292f7fb21d
jizhaogit <ji.zhao@telus.com>
2024-06-14 11:18:53 -0400

    Zenhub #1255 Edit Goal - My current goals

--- resources/views/components/xdropdown.blade.php
+++ resources/views/components/xdropdown.blade.php
@@ -2,8 +2,8 @@
 <label>
     {{ __($label ?? '') }}
     <!-- {!! $label ?? '' ? ' <i id="tag-info" class="fas fa-info-circle" data-trigger=" click " data-toggle-select="popover" data-placement="right" data-html="true" data-content="Tags help to more accurately identity, sort, and report on your goals. You can add more than one tag to a goal. The list of tags will change and grow over time. <br/><br/><a href=\'/resource/goal-setting?t=5\' target=\'_blank\'><u>View full list of tag descriptions.</u></a>" data-original-title=""></i><br>': ''!!} -->
-    {!! $label ?? '' ? ' <i id="tag-info" class="fas fa-info-circle" data-trigger=" click " data-toggle-select="popover" data-placement="right" data-html="true" data-content="Tags help to more accurately identity, sort, and report on your goals. You can add more than one tag to a goal. The list of tags will change and grow over time. <br/><br/><a href=\'/resources/goal-setting?t=8\' target=\'_blank\'><u>View full list of tag descriptions.</u></a>" data-original-title=""></i><br>': ''!!}
-    
+    {!! $label ?? '' ? ' <i id="tag-info" class="fas fa-info-circle" data-trigger=" focus " data-toggle-select="popover" data-placement="right" data-html="true" data-content="Tags help to more accurately identity, sort, and report on your goals. You can add more than one tag to a goal. The list of tags will change and grow over time. <br/><br/><a href=\'/resources/goal-setting?t=8\' target=\'_blank\'><u>View full list of tag descriptions.</u></a>" data-original-title=""></i><br>': ''!!}
+    <span id="tagLabel" class="sr-only">goal tags field</span>
     <select {!! $attributes->merge(['class' => 'form-control']) !!}>
         @if($blankOptionText != null)
             <option value="" >{{$blankOptionText}}</option>
