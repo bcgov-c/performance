9f9e51bf87cf48459f69701379a8e2ccccc2cd7a
jizhaogit <ji.zhao@telus.com>
2024-07-03 09:32:33 -0400

    Zenhub #1268 Switch Identity page does not clear the filters when loaded

--- resources/views/sysadmin/switch-identity/partials/filter.blade.php
+++ resources/views/sysadmin/switch-identity/partials/filter.blade.php
@@ -46,13 +46,14 @@
             <label for="criteria">Search Criteria</label>
             <select id="criteria" name="criteria" class="form-control">
                 @foreach( $criteriaList as $key => $value )
-                    <option value="{{ $key }}" {{  old('criteria') == $key ? 'selected' : '' }} >{{ $value }}</option>
+                    <option value="{{ $key }}"  >{{ $value }}</option>
                 @endforeach
             </select>
         </div>
         <div class="form-group col-md-2" id="search_text_group">
             <label for="search_text">Search Text</label>
-            <input type="text" id="search_text" name="search_text" class="form-control" value="{{ old('search_text') }}" placeholder="Search Text">
+            <input type="text" id="search_text" name="search_text" class="form-control" 
+                    value="" placeholder="Search Text">
         </div>
         <div class="form-group col-md-2 p-3 float-left float-bottom" style="display: flex; flex-direction: column;">
             <div class="form-group row"> </div>
