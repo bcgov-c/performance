e78496a44dc6c4088f733de25d4b0812c76710a9
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2024-07-16 13:33:54 -0700

    Revert "ZenHub #1297 - Additional Fixes"
    This reverts commit 23a24894afaea1ec6ddbdf60886a2ddda0f2b808.

--- database/migrations/2024_06_28_105053_add_id_to_employee_demo.php
+++ database/migrations/2024_06_28_105053_add_id_to_employee_demo.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('employee_demo', function (Blueprint $table) {
-            $table->id()->first();
+            $table->unsignedBigInteger('id', true)->first();
         });
     }
 
