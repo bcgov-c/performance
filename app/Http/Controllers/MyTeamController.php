8ac5477b711892f8a64ca333bbcd7fac1185a3a7
jizhaogit <ji.zhao@telus.com>
2024-07-25 10:37:57 -0400

    Revert "Zenhub #1331 Supervisor is unable to access employee profile through "My Team""
    This reverts commit 7a89abb8fd789687f05b5ab820569c3f153d0cf9.

--- app/Http/Controllers/MyTeamController.php
+++ app/Http/Controllers/MyTeamController.php
@@ -494,7 +494,7 @@ class MyTeamController extends Controller
         $can_access = false;
 
         $reporting_tos = array();
-        $reportingHierarchy = $this->reportingHierarchy($id);    
+        $reportingHierarchy = $this->reportingHierarchy($id, $reporting_tos);    
         
         if(in_array(Auth::id(), $reportingHierarchy)) {
             $can_access = true;
@@ -750,23 +750,21 @@ class MyTeamController extends Controller
     }
 
 
-    private function reportingHierarchy($user_id) {
-        $tree = [
-            'user_id' => $user_id,
-            'subordinates' => []
-        ];
+    private function reportingHierarchy($user_id, $reporting_tos){
+        if(!in_array($user_id, $reporting_tos)){
+            $reporting_to_userids = DB::table('users_annex')
+                        ->select('reporting_to_userid')
+                        ->where('user_id', $user_id)
+                        ->distinct()
+                        ->get() ;
 
-        $subordinates = DB::table('users_annex')
-            ->select('user_id')
-            ->where('reporting_to_userid', $user_id)
-            ->distinct()
-            ->get();
-
-        foreach ($subordinates as $subordinate) {
-            $tree['subordinates'][] = $this->reportingHierarchy($subordinate->user_id);
+            if(count($reporting_to_userids) > 0) {
+                $supervisor_userid = $reporting_to_userids[0]->reporting_to_userid;
+                array_push($reporting_tos, $supervisor_userid);
+                $reporting_tos = $this->reportingHierarchy($supervisor_userid, $reporting_tos);
+            } 
         }
-
-        return $tree;
+        return $reporting_tos;
     }
 
 }
