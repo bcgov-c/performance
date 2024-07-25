2039ce9206052c3ecfc6dc5f24a39977f67af22d
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2024-07-25 05:54:47 -0700

    Revert #1297 from Dev and Test
    Revert #1297 from Dev and Test

--- app/Console/Commands/GetODSEmployeeDemographics.php
+++ app/Console/Commands/GetODSEmployeeDemographics.php
@@ -143,74 +143,73 @@ class GetODSEmployeeDemographics extends Command
           $total += count($data);
           $this->info(Carbon::now()->format('c').' - $top = ' . $top . ' : $skip = ' . $skip . ' : $data = ' . count($data) . ' : Count = ' . $total);
 
-        foreach($data as $item){
-            EmployeeDemo::withoutGlobalScopes()
-            ->updateOrCreate(
-                [
-                    'employee_id' => $item['EMPLID'],
-                    'empl_record' => $item['EMPL_RCD'],
-                ],
-                [
-                    'employee_first_name' => $item['first_name'],
-                    'employee_last_name' => $item['last_name'],
-                    'employee_status' => $item['EMPL_STATUS'],
-                    'employee_email' => $item['email'],
-                    'classification_group' => $item['ClassificationGroup'],
-                    'deptid' => $item['DEPTID'],
-                    'jobcode' => $item['JOBCODE'],
-                    'job_title' => $item['jobcode_desc'],
-                    'position_number' => $item['position_number'],
-                    'position_start_date' => $item['position_start_date'] ? date('Y-m-d', strtotime($item['position_start_date'])) : null,
-                    'guid' => trim($item['GUID']),
-                    'date_deleted' => $item['date_deleted'] ? date('Y-m-d H:i:s', strtotime($item['date_deleted'])) : null,
-                    'date_updated' => $item['date_updated'] ? date('Y-m-d H:i:s', strtotime($item['date_updated'])) : null,
-                    'business_unit' => $item['BUSINESS_UNIT'],
-                    'effdt' => $item['EFFDT'],
-                    'effseq' => $item['EFFSEQ'],
-                    'empl_class' => $item['EMPL_CLASS'],
-                    'empl_ctg' => $item['EMPL_CTG'],
-                    'hire_dt' => $item['HIRE_DT'] ? date('Y-m-d', strtotime($item['HIRE_DT'])) : null,
-                    'idir' => $item['IDIR'],
-                    'job_function' => $item['JOB_FUNCTION'],
-                    'jobcodedescgroup' => $item['JobCodeDescGroup'],
-                    'occupationalgroup' => $item['OccupationalGroup'],
-                    'paygroup' => $item['PAYGROUP'],
-                    'organization' => trim($item['Organization']),
-                    'job_indicator' => $item['job_indicator'],
-                    'address1' => $item['address1'],
-                    'address2' => $item['address2'],
-                    'appointment_status' => $item['appointment_status'],
-                    'can_noc_code' => $item['can_noc_code'],
-                    'city' => $item['city'],
-                    'country' => $item['country'],
-                    'employee_status_long' => $item['employee_status_long_description'],
-                    'job_function_employee_group' => $item['job_function_employee_group'],
-                    'jobcode_desc' => $item['jobcode_desc'],
-                    'level1_program' => trim($item['level1_program']),
-                    'level2_division' => trim($item['level2_division']),
-                    'level3_branch' => trim($item['level3_branch']),
-                    'level4' => trim($item['level4']),
-                    'employee_middle_name' => $item['middle_name'],
-                    'employee_name' => $item['name'],
-                    'office_address' => $item['office_address1'],
-                    'office_address2' => $item['office_address2'],
-                    'office_city' => $item['office_city'],
-                    'office_country' => $item['office_country'],
-                    'office_location_code' => $item['office_location_code'],
-                    'office_phone' => $item['office_phone'],
-                    'office_postal' => $item['office_postal'],
-                    'office_stateprovince' => $item['office_stateprovince'],
-                    'phone' => $item['phone'],
-                    'postal' => $item['postal'],
-                    'public_service_act' => $item['public_service_act'],
-                    'sal_admin_plan' => $item['sal_admin_plan'],
-                    'stateprovince' => $item['stateprovince'],
-                    'supervisor_emplid' => $item['supervisor_emplid'],
-                    'supervisor_email' => $item['supervisor_email'],
-                    'supervisor_name' => $item['supervisor_name'],
-                    'supervisor_position_number' => $item['supervisor_position_number'],
-                    'tgb_reg_district' => $item['tgb_reg_district'],
-                ]
+          foreach($data as $item){
+            DB::table('employee_demo')->updateOrInsert(
+              [
+                'employee_id' => $item['EMPLID'],
+                'empl_record' => $item['EMPL_RCD'],
+              ],
+              [
+                'employee_first_name' => $item['first_name'],
+                'employee_last_name' => $item['last_name'],
+                'employee_status' => $item['EMPL_STATUS'],
+                'employee_email' => $item['email'],
+                'classification_group' => $item['ClassificationGroup'],
+                'deptid' => $item['DEPTID'],
+                'jobcode' => $item['JOBCODE'],
+                'job_title' => $item['jobcode_desc'],
+                'position_number' => $item['position_number'],
+                'position_start_date' => $item['position_start_date'] ? date('Y-m-d', strtotime($item['position_start_date'])) : null,
+                'guid' => trim($item['GUID']),
+                'date_deleted' => $item['date_deleted'] ? date('Y-m-d H:i:s', strtotime($item['date_deleted'])) : null,
+                'date_updated' => $item['date_updated'] ? date('Y-m-d H:i:s', strtotime($item['date_updated'])) : null,
+                'business_unit' => $item['BUSINESS_UNIT'],
+                'effdt' => $item['EFFDT'],
+                'effseq' => $item['EFFSEQ'],
+                'empl_class' => $item['EMPL_CLASS'],
+                'empl_ctg' => $item['EMPL_CTG'],
+                'hire_dt' => $item['HIRE_DT'] ? date('Y-m-d H:i:s', strtotime($item['HIRE_DT'])) : null,
+                'idir' => $item['IDIR'],
+                'job_function' => $item['JOB_FUNCTION'],
+                'jobcodedescgroup' => $item['JobCodeDescGroup'],
+                'occupationalgroup' => $item['OccupationalGroup'],
+                'paygroup' => $item['PAYGROUP'],
+                'organization' => trim($item['Organization']),
+                'job_indicator' => $item['job_indicator'],
+                'address1' => $item['address1'],
+                'address2' => $item['address2'],
+                'appointment_status' => $item['appointment_status'],
+                'can_noc_code' => $item['can_noc_code'],
+                'city' => $item['city'],
+                'country' => $item['country'],
+                'employee_status_long' => $item['employee_status_long_description'],
+                'job_function_employee_group' => $item['job_function_employee_group'],
+                'jobcode_desc' => $item['jobcode_desc'],
+                'level1_program' => trim($item['level1_program']),
+                'level2_division' => trim($item['level2_division']),
+                'level3_branch' => trim($item['level3_branch']),
+                'level4' => trim($item['level4']),
+                'employee_middle_name' => $item['middle_name'],
+                'employee_name' => $item['name'],
+                'office_address' => $item['office_address1'],
+                'office_address2' => $item['office_address2'],
+                'office_city' => $item['office_city'],
+                'office_country' => $item['office_country'],
+                'office_location_code' => $item['office_location_code'],
+                'office_phone' => $item['office_phone'],
+                'office_postal' => $item['office_postal'],
+                'office_stateprovince' => $item['office_stateprovince'],
+                'phone' => $item['phone'],
+                'postal' => $item['postal'],
+                'public_service_act' => $item['public_service_act'],
+                'sal_admin_plan' => $item['sal_admin_plan'],
+                'stateprovince' => $item['stateprovince'],
+                'supervisor_emplid' => $item['supervisor_emplid'],
+                'supervisor_email' => $item['supervisor_email'],
+                'supervisor_name' => $item['supervisor_name'],
+                'supervisor_position_number' => $item['supervisor_position_number'],
+                'tgb_reg_district' => $item['tgb_reg_district'],
+              ]
             );
             if($alldataoverride) {
                 DB::table('ods_employee_demo')->updateOrInsert(
@@ -237,7 +236,7 @@ class GetODSEmployeeDemographics extends Command
                     'effseq' => $item['EFFSEQ'],
                     'empl_class' => $item['EMPL_CLASS'],
                     'empl_ctg' => $item['EMPL_CTG'],
-                    'hire_dt' => $item['HIRE_DT'] ? date('Y-m-d', strtotime($item['HIRE_DT'])) : null,
+                    'hire_dt' => $item['HIRE_DT'] ? date('Y-m-d H:i:s', strtotime($item['HIRE_DT'])) : null,
                     'idir' => $item['IDIR'],
                     'job_function' => $item['JOB_FUNCTION'],
                     'jobcodedescgroup' => $item['JobCodeDescGroup'],
@@ -347,7 +346,7 @@ class GetODSEmployeeDemographics extends Command
             $q->whereNotExists(function ($query) {
                 return $query->select(DB::raw(1))
                         ->from('excluded_departments')
-                        ->where('excluded_departments.deptid', 'employee_demo.deptid')
+                        ->whereColumn('employee_demo.deptid', 'excluded_departments.deptid')
                         ->whereNull('excluded_departments.deleted_at');
             });    
         })
@@ -359,7 +358,7 @@ class GetODSEmployeeDemographics extends Command
             $q->whereExists(function ($query) {
                 return $query->select(DB::raw(1))
                         ->from('excluded_departments')
-                        ->where('excluded_departments.deptid', 'employee_demo.deptid')
+                        ->whereColumn('employee_demo.deptid', 'excluded_departments.deptid')
                         ->whereNull('excluded_departments.deleted_at');
             });    
         })
@@ -389,29 +388,6 @@ class GetODSEmployeeDemographics extends Command
         })
         ->update(['pdp_excluded' => 1]);
         $this->info(Carbon::now()->format('c').' - Dept Exclusions updated.');
-
-        if($alldataoverride) {
-            if(ODSEmployeeDemo::first()){
-                $this->info(Carbon::now()->format('c').' - Checking for missing Employee Demo records...');
-                $lostItems = EmployeeDemo::withoutGlobalScopes()
-                ->select('id', 'employee_demo.employee_id', 'employee_demo.empl_record', 'employee_demo.date_deleted')
-                ->whereNull('employee_demo.date_deleted')
-                ->whereNotExists(function($subq){
-                    return $subq->select(DB::raw(1))
-                    ->from('ods_employee_demo')
-                    ->whereRaw('ods_employee_demo.employee_id = employee_demo.employee_id and ods_employee_demo.empl_record = employee_demo.empl_record');
-                })
-                ->get();
-                $lostItems->each(function($item, $key){
-                    $this->info(Carbon::now()->format('c')." ... missing {$item->employee_id} / {$item->empl_record} - updating date_deleted to current date/time...");
-                    $item->date_deleted = now();
-                    $item->save();
-                });
-                $this->info(Carbon::now()->format('c').' - Completed check for missing Employee Demo records...');
-            } else {
-                $this->info(Carbon::now()->format('c').' - Skipping check for missing Employee Demo records...');
-            }
-        }
 
         if(!$nodateupdateoverride) {
             DB::table('stored_dates')->updateOrInsert(
