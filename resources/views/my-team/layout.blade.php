bc53ed3e007e1d123fab5a1ccbfa432d3625c953
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2024-07-26 15:28:45 -0700

    ZenHub #1337 - Missing code change from #1155
    ZenHub #1337 - Missing code change from #1155

--- resources/views/my-team/layout.blade.php
+++ resources/views/my-team/layout.blade.php
@@ -91,7 +91,7 @@
                         },
                 width: '100%',
                 ajax: {
-                    url: '/user-options',
+                    url: '/user-options', 
                     dataType: 'json',
                     data: function (params) {
                         const query = {
