c0b802f669f1e9d1d5298a29fe722cc80d0f4336
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2024-08-02 10:36:41 -0700

    ZenHub #1293 - UptimeRobot Monitoring
    ZenHub #1293 - UptimeRobot Monitoring

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'ViewShare' => \App\Http\Middleware\ViewShare::class,
         'ViewAsPermission' => \App\Http\Middleware\ViewAsPermission::class,
+        'api_token' => \App\Http\Middleware\ApiToken::class,
     ];
 }
