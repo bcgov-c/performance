da1d1df28ea1efed57e0cf171e5793326a7dd419
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2024-08-22 16:12:18 -0700

    ZenHub #1358 - Add tables to ExportDatabaseToBI process
    ZenHub #1358 - Add tables to ExportDatabaseToBI process

--- app/Console/Commands/ExportDatabaseToBI.php
+++ app/Console/Commands/ExportDatabaseToBI.php
@@ -42,7 +42,7 @@ class ExportDatabaseToBI extends Command
         ['name' => 'users',                         'delta' => 'updated_at',    'hidden' => ['password', 'remember_token'] ],
         ['name' => 'user_reporting_tos',            'delta' => 'updated_at',    'hidden' => null ],
         ['name' => 'preferred_supervisor',          'delta' => 'updated_at',    'hidden' => null ],
-        ['name' => 'employee_managers',             'delta' => 'updated_at',    'hidden' => null ],
+        ['name' => 'employee_managers',             'delta' => null,            'hidden' => null ],
         ['name' => 'users_annex',                   'delta' => 'updated_at',    'hidden' => null ],
         ['name' => 'ods_dept_org_hierarchy',        'delta' => 'updated_at',    'hidden' => null ],
     ];
