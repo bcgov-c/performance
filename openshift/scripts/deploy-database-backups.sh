71fcf3985dc0d493342c9416c1c5b021ff1f73e1
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-09-09 15:00:08 -0700

    Removed helm --timeout

--- openshift/scripts/deploy-database-backups.sh
+++ openshift/scripts/deploy-database-backups.sh
@@ -63,6 +63,6 @@ else
       ENVIRONMENT_FRIENDLY_NAME:
         value: \"$APP_NAME Backups\"
     " > config.yaml
-  helm install $DB_BACKUP_DEPLOYMENT_NAME $BACKUP_HELM_CHART --atomic --wait --timeout 30 -f config.yaml
+  helm install $DB_BACKUP_DEPLOYMENT_NAME $BACKUP_HELM_CHART --atomic --wait -f config.yaml
   oc set image deployment/$DB_BACKUP_DEPLOYMENT_NAME backup-storage=$DB_BACKUP_IMAGE
 fi
