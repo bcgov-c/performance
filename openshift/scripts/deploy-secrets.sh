45e5f39d36a1bed32699af870cf766c29095cf86
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-09-12 12:03:04 -0700

    Adjust secret deployment, removed delete on discovery

--- openshift/scripts/deploy-secrets.sh
+++ openshift/scripts/deploy-secrets.sh
@@ -5,14 +5,10 @@ echo "Deploying secrets to: $OC_PROJECT..."
 # Check if the Helm deployment exists
 if [[ `oc describe secret $APP_NAME-secrets 2>&1` =~ "NotFound" ]]; then
   echo "Secrets not found."
-else
-  echo "Secrets already exist ($APP_NAME-secrets). Deleting..."
-  oc delete secret $APP_NAME-secrets
-fi
 
-echo "Creating... $APP_NAME-secrets"
+  echo "Creating... $APP_NAME-secrets"
 
-echo "
+  echo "
     kind: Secret
     apiVersion: v1
     metadata:
@@ -27,3 +23,10 @@ echo "
       type: Opaque
     " > secrets.yml
   oc create -f secrets.yml
+
+else
+  echo "Secrets already exist ($APP_NAME-secrets)."
+
+  # echo "Deleting secrets..."
+  # oc delete secret $APP_NAME-secrets
+fi
