288d5832e2bf978843efd2eaa44beb9ed5befffa
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-09-16 13:55:21 -0700

    Changed cron startup dir to /var/www/html Moved nohup command from cron.sh script to template.json

--- openshift/config/cron/cron.sh
+++ openshift/config/cron/cron.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd /var/www
+cd /var/www/html
 
 echo "List of Scheduled Jobs:"
 php artisan schedule:list
@@ -10,7 +10,7 @@ do
   # Run Scheduled Jobs
   date_time=$(date +"%Y-%m-%d %H:%M:%S")
   echo "Running Scheduled Jobs [$date_time]"
-  nohup php artisan schedule:run
+  php artisan schedule:run --verbose --no-interaction </dev/null>/dev/null 2>&1 &
   # nohup php artisan queue:work  --daemon --tries=3 --timeout=0 > ./storage/logs/queue-work.log </dev/null>/dev/null 2>&1 &
   #nohup php artisan schedule:work --verbose --no-interaction </dev/null>/dev/null 2>&1 &
   sleep 60
