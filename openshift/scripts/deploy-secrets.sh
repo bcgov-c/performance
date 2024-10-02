d0ef874df3fc6f230324e8308fc0310e06106acf
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-10-02 09:50:15 -0700

    Fix indentation in deploy-secrets

--- openshift/scripts/deploy-secrets.sh
+++ openshift/scripts/deploy-secrets.sh
@@ -8,22 +8,22 @@ if [[ `oc describe secret $APP_NAME-secrets 2>&1` =~ "NotFound" ]]; then
 
   echo "Creating... $APP_NAME-secrets"
 
-  cat <<EOF > secrets.yml
-  kind: Secret
-  apiVersion: v1
-  metadata:
-    name: $APP_NAME-secrets
-    namespace: $OC_PROJECT
-    labels:
-      template: $APP_NAME
-  stringData:
-    database-name: $DB_DATABASE
-    database-password: $SECRET_DB_PASSWORD
-    database-user: $DB_USER
-  type: Opaque
-  EOF
+cat <<EOF > secrets.yml
+kind: Secret
+apiVersion: v1
+metadata:
+  name: $APP_NAME-secrets
+  namespace: $OC_PROJECT
+  labels:
+    template: $APP_NAME
+stringData:
+  database-name: $DB_DATABASE
+  database-password: $SECRET_DB_PASSWORD
+  database-user: $DB_USER
+type: Opaque
+EOF
 
-  oc create -f secrets.yml
+oc create -f secrets.yml
 
 else
   echo "Secrets already exist ($APP_NAME-secrets)."
