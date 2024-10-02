3ca359fed0f7494aa254b8b6300838670e8dab1c
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-10-02 11:50:54 -0700

    Adjust healthcheck timeout in docker-compose Renove data dir (related to local redis dump), added to gitignore Refactored deploy-secrets shell

--- openshift/scripts/deploy-secrets.sh
+++ openshift/scripts/deploy-secrets.sh
@@ -5,8 +5,14 @@ echo "Deploying secrets to: $OC_PROJECT..."
 # Check if the Helm deployment exists
 if [[ `oc describe secret $APP_NAME-secrets 2>&1` =~ "NotFound" ]]; then
   echo "Secrets not found."
-
   echo "Creating... $APP_NAME-secrets"
+
+else
+  echo "Secrets already exist ($APP_NAME-secrets)."
+  echo "Deleting secrets..."
+  oc delete secret $APP_NAME-secrets
+  echo "Recreating... $APP_NAME-secrets"
+fi
 
 cat <<EOF > secrets.yml
 kind: Secret
@@ -24,10 +30,3 @@ type: Opaque
 EOF
 
 oc create -f secrets.yml
-
-else
-  echo "Secrets already exist ($APP_NAME-secrets)."
-
-  # echo "Deleting secrets..."
-  # oc delete secret $APP_NAME-secrets
-fi
