c998f8d5ee287e0f60f9392c22423c369b3b5c6b
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-10-18 16:03:31 -0700

    Replaced REDIS_IMAGE with REDIS_HELM_CHART Add helm test to deploy-redis-sentinel.sh

--- openshift/scripts/deploy-redis-sentinel.sh
+++ openshift/scripts/deploy-redis-sentinel.sh
@@ -5,6 +5,7 @@ oc project $OC_PROJECT
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
+# Create a temporary values file
 cat > values.yml<<EOF
 global:
   redis:
@@ -15,6 +16,34 @@ sentinel:
   enabled: true
 EOF
 
-helm install $REDIS_NAME-sentinel bitnami/redis --values values.yml
+# Check if the Helm deployment exists
+if helm list -q | grep -q "^$REDIS_NAME$"; then
+  echo "Helm deployment found. Updating..."
 
+  # Upgrade the Helm deployment with the new values
+  if [[ `helm upgrade $REDIS_NAME $REDIS_HELM_CHART --reuse-values -f values.yml 2>&1` =~ "Error" ]]; then
+    echo "❌ Helm upgrade FAILED."
+    exit 1
+  fi
+
+  if [[ `oc describe deployment $REDIS_NAME 2>&1` =~ "NotFound" ]]; then
+    echo "Helm deployment ($REDIS_NAME) exists, but NOT FOUND."
+    exit 1
+  fi
+else
+  echo "Helm $REDIS_NAME NOT FOUND. Beginning deployment..."
+
+  helm install $REDIS_NAME-sentinel $REDIS_HELM_CHART --values values.yml
+
+  # Set best-effort resource limits for the deployment
+  echo "Setting best-effort resource limits for the deployment..."
+  oc set resources deployment/$REDIS_NAME --limits=cpu=0,memory=0 --requests=cpu=0,memory=0
+fi
+
+echo "Helm updates completed for $REDIS_NAME."
+
+# Clean up the temporary values file
+rm values.yaml
+
+e ho "Deploying Redis Insight..."
 oc apply -f ./openshift/redis-insight.yml
